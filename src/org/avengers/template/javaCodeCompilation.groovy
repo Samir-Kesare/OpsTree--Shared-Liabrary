1f7749bc76fbc8e6e6ce0ea78dc19c89cd6a1c4c
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-02-16 01:57:42 +0530

    Update javaCodeCompilation.groovy

--- src/org/avengers/template/javaCodeCompilation.groovy
+++ src/org/avengers/template/javaCodeCompilation.groovy
@@ -1,1 +1,16 @@
+package org.avengers.template
 
+import org.avengers.common.gitCheckout
+import org.avengers.common.cleanWorkspace
+import org.avengers.java.compile.*
+
+  def call(String url, String branch){
+    gitCheckout = new gitCheckout()
+    javaCompile = new compile()
+    cleanWorkspace = new cleanWorkspace()
+    gitCheckout.call(branch: branch, url: url)
+    javaCompile.call()
+    cleanWorkspace.call()
+  }
+
+  
