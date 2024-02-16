ebcee7896c1986970d27d0e9223a45dfd1ca60d5
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-02-16 14:01:33 +0530

    Update DslJob.groovy

--- src/org/avengers/genericCi/slackNotification/DslJob.groovy
+++ src/org/avengers/genericCi/slackNotification/DslJob.groovy
@@ -1,1 +1,19 @@
+package org.avengers.genericCi.slackNotification
 
+def call() {
+    stage('Build Dsl Job') {
+        script {
+            // Define the DSL for creating a Freestyle job
+            def jobDSL = '''
+                job('Freestyle-Job') {
+                    description('This is a sample Freestyle job created using a Scripted Pipeline')
+                    steps {
+                        shell('echo "Hello, world!"')
+                    }
+                }
+            '''
+            // Execute the job DSL to create the Freestyle job
+            jobDsl(scriptText: jobDSL)
+        }
+    }
+}
