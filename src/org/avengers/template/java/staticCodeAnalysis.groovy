f59ed6448e2e5895b15fca994108df27107e44ee
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-02-16 18:55:14 +0530

    Update staticCodeAnalysis.groovy

--- src/org/avengers/template/java/staticCodeAnalysis.groovy
+++ src/org/avengers/template/java/staticCodeAnalysis.groovy
@@ -8,27 +8,12 @@ import org.avengers.java.staticCodeAnalysis.*
 def call(Map config = [:]){
     def gitCheckout = new gitCheckout()
     def javaCompile = new compile()
-    def cleanWorkspace = new cleanWorkspace()
     def staticCodeAnalysis = new staticCodeAnalysis()
-    try{
+    def cleanWorkspace = new cleanWorkspace()
+
     gitCheckout.call(branch: config.branch, url: config.url  )
     javaCompile.call()
     staticCodeAnalysis.call()
-    }
-    // gitCheckout.call(branch: config.branch, url: config.url  )
-    // javaCompile.call()
-    catch (e) {
-        echo 'Analysis Failed'
-        cleanWorkspace.call()
-        // Since we're catching the exception in order to report on it,
-        // we need to re-throw it, to ensure that the build is marked as failed
-        throw e
-    } 
-    finally {
-         def currentResult = currentBuild.result ?: 'SUCCESS'
-        if ((currentResult == 'UNSTABLE')||(currentResult == 'ABORTED')) {
-        cleanWorkspace.call()
-            // echo 'This will run only if the run was marked as unstable'
-    }
+    cleanWorkspace.call()
   }
 }
