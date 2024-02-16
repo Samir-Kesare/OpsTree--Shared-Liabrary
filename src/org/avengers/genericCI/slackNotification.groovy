e7ec96f7f8e0b6dc839e61a551b25d3384819de0
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-02-16 13:24:57 +0530

    Update slackNotification.groovy

--- src/org/avengers/genericCI/slackNotification.groovy
+++ src/org/avengers/genericCI/slackNotification.groovy
@@ -1,7 +1,14 @@
-
 package org.avengers.genericCI
 
-def call() {
-    stage ('Send Slack Notification') {
-       script {
-         
+def call(status) {
+    stage('Send Slack Notification') {
+        script {
+            if (status == 'SUCCESS') {
+                slackSend(channel: 'jenkinss', color: '#00FF00', message: 'Job Build successfully')
+            }
+            if (status == 'FAILURE') {
+                slackSend(channel: 'jenkinss', color: '#FF0000', message: 'Job Failed')
+            }
+        }
+    }
+}
