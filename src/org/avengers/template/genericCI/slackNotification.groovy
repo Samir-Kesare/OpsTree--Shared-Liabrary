8e7c21006a5e09f55d93d0197b0aff9673b9432b
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-02-16 13:35:53 +0530

    Update slackNotification.groovy

--- src/org/avengers/template/genericCI/slackNotification.groovy
+++ src/org/avengers/template/genericCI/slackNotification.groovy
@@ -1,1 +1,9 @@
+package org.avengers.template.genericCI
 
+import org.avengers.genericCI.*
+
+ def call(String url, String creds, String branch){
+   slackNotification = new SlackNotification()
+
+   slackNotification.call()
+ }
