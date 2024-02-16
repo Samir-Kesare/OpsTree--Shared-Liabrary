ddf3c5e25ba7f4f114cc709bd0228b29e536143d
Ninja-07 <156056709+Parasharam-Desai@users.noreply.github.com>
2024-02-16 20:41:13 +0530

    Update test.groovy

--- src/org/avengers/java/unitTesting/test.groovy
+++ src/org/avengers/java/unitTesting/test.groovy
@@ -1,1 +1,11 @@
+package org.avengers.java.unitTesting
 
+def call(){           
+        stage("Unit Testing ") {
+                script {
+                   
+                    sh 'mvn test'
+                }
+            }
+}
+      
