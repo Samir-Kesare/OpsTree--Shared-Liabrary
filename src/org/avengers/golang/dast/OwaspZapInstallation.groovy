3f4c1ca4a4182fc38c3a614c73dcdfcdbdd76c86
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-02-16 16:45:11 +0530

    Update and rename OwaspZapInstallation.groovy to OwaspZapInstallation.groovy

---
+++ src/org/avengers/golang/dast/OwaspZapInstallation.groovy
@@ -0,0 +1,12 @@
+package org.avengers.golang.dast
+def call() {
+  stage('Install ZAP') {
+    script {
+         // Download and install OWASP ZAP
+          sh 'wget https://github.com/zaproxy/zaproxy/releases/download/v${zapVersion}/ZAP_${zapVersion}_Linux.tar.gz'
+          sh 'tar -xvf ZAP_${zapVersion}_Linux.tar.gz'
+                    
+                }
+            }
+        }
+
