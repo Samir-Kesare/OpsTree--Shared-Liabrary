b6c03a7a5bdd76492ec5547aded6bab8ea0c3680
Ninja-07 <156056709+Parasharam-Desai@users.noreply.github.com>
2024-02-16 22:22:08 +0530

    Update bug.groovy

--- src/org/avengers/java/bugAnalysis/bug.groovy
+++ src/org/avengers/java/bugAnalysis/bug.groovy
@@ -2,12 +2,10 @@ package org.avengers.java.bugAnalysis
 
 def call(){           
         stage("Bug Analysis ") {
-            steps {
                 script {
                     sh 'mvn compile'
                     sh 'mvn spotbugs:spotbugs'
                     sh 'mvn site'
                 }
-            }
         }
 }
