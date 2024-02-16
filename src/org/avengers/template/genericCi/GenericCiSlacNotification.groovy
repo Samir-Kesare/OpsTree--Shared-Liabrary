cf0328dcb18ebaab5b6ceea4218679e0344c4f8b
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-02-16 14:13:17 +0530

    Update GenericCiSlacNotification.groovy

--- src/org/avengers/template/genericCi/GenericCiSlacNotification.groovy
+++ src/org/avengers/template/genericCi/GenericCiSlacNotification.groovy
@@ -1,1 +1,11 @@
+package org.avengers.template.genericCi
 
+import org.avengers.genericCi.slackNotification.*
+
+def call(String url, String creds, String branch){
+  dslJob = new DslJob()
+  sendNotification = new SendNotification()
+
+  dslJob .call()
+  sendNotification.call()
+}
