074ae2bd61f1d48988d5e53e753cbb0718db2e61
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-02-16 17:07:39 +0530

    Update GolangDast.groovy

--- src/org/avengers/template/golang/GolangDast.groovy
+++ src/org/avengers/template/golang/GolangDast.groovy
@@ -1,1 +1,14 @@
+package org.avengers.template.golang
 
+import org.avengers.common.*
+import org.avengers.golang.dast.*
+
+def call(String url, String creds, String branch, String zapVersion){
+  gitCheckoutPrivate = new GitCheckoutPrivate()
+  OwaspZapInstallation = new OwaspZapInstallation()
+  RunZap = new RunZap()
+
+  gitCheckoutPrivate.call(url, creds, branch)
+  OwaspZapInstallation.call(zapVersion)
+  RunZap.call(zapVersion)
+}
