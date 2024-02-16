96c5eed179e0c5dac0ba6f73489a1b34fce69cb7
Ninja-07 <156056709+Parasharam-Desai@users.noreply.github.com>
2024-02-16 20:40:02 +0530

    Update bug.groovy

--- src/org/avengers/java/bugAnalysis/bug.groovy
+++ src/org/avengers/java/bugAnalysis/bug.groovy
@@ -10,20 +10,4 @@ def call(){
                 }
             }
         }
-                                
-                                stage('Publish HTML Report') {
-                                    steps {
-                                        script {
-                                            publishHTML([
-                                                allowMissing: false,
-                                                alwaysLinkToLastBuild: true,
-                                                keepAll: true,
-                                                reportDir: 'target/site',
-                                                reportFiles: 'spotbugs.html',
-                                                reportName: 'SpotBugs Report'
-                                            ])
-                                        }
-                                    }
-                                }
-                            }
-                        }
+        
