ad601b416f95e3b5f2adf0db778a28a6d37a689e
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-02-16 20:14:05 +0530

    Update SendNotification.groovy

--- src/org/avengers/genericCi/slackNotification/SendNotification.groovy
+++ src/org/avengers/genericCi/slackNotification/SendNotification.groovy
@@ -1,10 +1,10 @@
 package org.avengers.genericCi.slackNotification
 
 def call() {
-    if (buildStatus == 'FAILURE') {
-        slackSend channel: 'jenkinss', message: 'Job Build successfully'
-    } else {
+    def status = env.BUILD_STATUS ?: 'SUCCESS' // Default to 'SUCCESS' if BUILD_STATUS is not set
+    if (status == 'FAILURE') {
         slackSend channel: 'jenkinss', message: 'Job Failed'
+    } else if (status == 'SUCCESS') {
+        slackSend channel: 'jenkinss', message: 'Job Build successfully'
     }
 }
-
