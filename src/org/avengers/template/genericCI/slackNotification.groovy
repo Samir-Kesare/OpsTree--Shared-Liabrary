cef8260d7935b8cfa56be34ecdb8e181a4ccbaf1
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-02-16 13:46:06 +0530

    Delete src/org/avengers/template/genericCI/slackNotification.groovy

--- src/org/avengers/template/genericCI/slackNotification.groovy
+++
@@ -1,9 +0,0 @@
-package org.avengers.template.genericCI
-
-import org.avengers.genericCI.*
-
- def call(String url, String creds, String branch){
-   slackNotification = new SlackNotification()
-
-   slackNotification.call()
- }
