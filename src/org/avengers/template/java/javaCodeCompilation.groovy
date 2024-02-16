5459da8d60e91833471c422f5d0ef2b996cd066f
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-02-16 15:34:19 +0530

    Update javaCodeCompilation.groovy

--- src/org/avengers/template/java/javaCodeCompilation.groovy
+++ src/org/avengers/template/java/javaCodeCompilation.groovy
@@ -8,9 +8,24 @@ def call(Map config = [:]){
     def gitCheckout = new gitCheckout()
     def javaCompile = new compile()
     // def cleanWorkspace = new cleanWorkspace()
+    try{
+    gitCheckout.call(branch: config.branch, url: config.url  )
+    javaCompile.call()   
+    }
     gitCheckout.call(branch: config.branch, url: config.url  )
     javaCompile.call()
-    // cleanWorkspace.call()
+    catch (e) {
+        echo 'Compilation Failed'
+        cleanWorkspace.call()
+        // Since we're catching the exception in order to report on it,
+        // we need to re-throw it, to ensure that the build is marked as failed
+        throw e
+    } 
+    finally {
+         def currentResult = currentBuild.result ?: 'SUCCESS'
+        if ((currentResult == 'UNSTABLE')||(currentResult == 'ABORTED')) {
+        cleanWorkspace.call()
+            // echo 'This will run only if the run was marked as unstable'
+    }
   }
-
-  
+}
