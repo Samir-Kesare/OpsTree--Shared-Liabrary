73579e26ca3f2a9791a159f3ffe4588dd7c8aa00
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-02-17 16:47:07 +0530

    Update OwaspZapInstallation.groovy

--- src/org/avengers/golang/dast/OwaspZapInstallation.groovy
+++ src/org/avengers/golang/dast/OwaspZapInstallation.groovy
@@ -3,8 +3,8 @@ def call(String zapVersion) {
   stage('Install ZAP') {
     script {
          // Download and install OWASP ZAP
-          sh 'wget https://github.com/zaproxy/zaproxy/releases/download/v${zapVersion}/ZAP_${zapVersion}_Linux.tar.gz'
-          sh 'tar -xvf ZAP_${zapVersion}_Linux.tar.gz'
+          sh "wget https://github.com/zaproxy/zaproxy/releases/download/v${zapVersion}/ZAP_${zapVersion}_Linux.tar.gz"
+          sh "tar -xvf ZAP_${zapVersion}_Linux.tar.gz"
                     
                 }
             }
