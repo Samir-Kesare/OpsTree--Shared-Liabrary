e0ece9eb9ae84989c18785f6c8605a6fd19c5e93
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-02-17 12:20:50 +0530

    Update GenericCiAmi.groovy

--- src/org/avengers/template/genericCi/GenericCiAmi.groovy
+++ src/org/avengers/template/genericCi/GenericCiAmi.groovy
@@ -1,1 +1,11 @@
+package org.avengers.template.genericCi
 
+import org.avengers.genericCi.packerAmi.*
+
+def call(){
+  packerInit = new PackerInit()
+  packerBuild = new PackerBuild()
+
+  packerInit .call()
+  packerBuild.call()
+}
