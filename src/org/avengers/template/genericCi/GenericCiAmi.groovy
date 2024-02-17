b309212b21b4e1f7bd9ff096aa282b186aa2e9b2
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-02-17 15:55:44 +0530

    Update GenericCiAmi.groovy

--- src/org/avengers/template/genericCi/GenericCiAmi.groovy
+++ src/org/avengers/template/genericCi/GenericCiAmi.groovy
@@ -2,11 +2,13 @@ package org.avengers.template.genericCi
 
 import org.avengers.genericCi.packerAmi.*
 
-def call() {
+def call(String url, String creds, String branch){
+    gitCheckoutPrivate = new GitCheckoutPrivate()
     def awsCred = new AwsCreds()
     def packerInit = new PackerInit()
     def packerBuild = new PackerBuild()
 
+    gitCheckoutPrivate.call(url, creds, branch)
     awsCred.awsCredentials()  // Call the correct method
     packerInit.call()
     packerBuild.call()
