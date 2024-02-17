c824fdf44dd62e6e3523007d986385afd06e2640
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-02-17 14:54:16 +0530

    Update PackerBuild.groovy

--- src/org/avengers/genericCi/packerAmi/PackerBuild.groovy
+++ src/org/avengers/genericCi/packerAmi/PackerBuild.groovy
@@ -4,15 +4,14 @@ def call() {
     // Change to the directory containing the Packer configuration
     dir('/home/shreya/') {
         // Initialize Packer (if necessary)
-        sh '''
-            withCredentials([[
-                $class: 'AmazonWebServicesCredentialsBinding',
-                credentialsId: 'aws-credentials-id',
-                accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-                secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
-            ]]) {
-                /usr/bin/packer build .
-            '''
+        withCredentials([[
+            $class: 'AmazonWebServicesCredentialsBinding',
+            credentialsId: 'aws-credentials-id',
+            accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+            secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+        ]]) {
+            sh '/usr/bin/packer build .'
+        }
     }
 }
 
