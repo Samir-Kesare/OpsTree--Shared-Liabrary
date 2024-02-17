865efb077fbae16523ebafebecc4295c0f2f7bc8
vyadavP7 <156056349+vyadavP7@users.noreply.github.com>
2024-02-18 02:26:07 +0530

    Update dependencyCheck.groovy

--- src/org/avengers/template/java/dependencyCheck.groovy
+++ src/org/avengers/template/java/dependencyCheck.groovy
@@ -12,7 +12,7 @@ def call(Map config = [:]){
   
     try {
         // Clone repository 
-        gitCheckout.call(branch: config.branch, url: config.url, credentialsId: config.creds)
+        gitCheckout.call(branch: branch, url: url, credentialsId: creds)
       
         // Package artifacts
         // packageArtifacts.call()
