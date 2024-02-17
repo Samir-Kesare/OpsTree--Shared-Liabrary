81da5df870544de8995b7459787abb2446b58e10
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-02-17 09:01:20 +0530

    Update RunZap.groovy

--- src/org/avengers/golang/dast/RunZap.groovy
+++ src/org/avengers/golang/dast/RunZap.groovy
@@ -3,7 +3,7 @@ def call() {
 stage('Run ZAP Scan') {
             steps {
                  // Start ZAP and perform the scan
-                  sh "/var/lib/jenkins/workspace/'Declarative Pipeline GoLang DAST'/ZAP_$(zapVersion)/zap.sh -cmd -port 8090 -quickurl http://18.183.109.200:8080/api/v1/employee/health -quickprogress -quickout out2.html"
+                  sh "/var/lib/jenkins/workspace/'Declarative Pipeline GoLang DAST'/ZAP_$(zapVersion)/zap.sh -cmd -port 8090 -quickurl http://43.207.90.113:8080/api/v1/employee/health -quickprogress -quickout out2.html"
                 }
             }
         }
