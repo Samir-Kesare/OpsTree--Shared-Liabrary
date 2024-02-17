4b6965f6c9efb83610f0d022f53fa97367ea918d
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-02-17 12:26:20 +0530

    Update PackerBuild.groovy

--- src/org/avengers/genericCi/packerAmi/PackerBuild.groovy
+++ src/org/avengers/genericCi/packerAmi/PackerBuild.groovy
@@ -1,5 +1,10 @@
 package org.avengers.genericCi.packerAmi
 
 def call() {
-    
+    // Change to the directory containing the Packer configuration
+    dir('/home/shreya/') {
+        // Build the AMI
+        sh '/usr/bin/packer build .'
+    }
 }
+
