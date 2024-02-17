0787fe33012958a5b7280e29a4d787c5875457ca
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-02-17 13:39:04 +0530

    Update GenericCiAmi.groovy

--- src/org/avengers/template/genericCi/GenericCiAmi.groovy
+++ src/org/avengers/template/genericCi/GenericCiAmi.groovy
@@ -1,15 +1,13 @@
 package org.avengers.template.genericCi
 
-import org.avengers.genericCi.packerAmi.AwsCreds 
-import org.avengers.genericCi.packerAmi.PackerInit
-import org.avengers.genericCi.packerAmi.PackerBuild
+import org.avengers.genericCi.packerAmi.*
 
-def call(){
-  def awsCred = new AwsCreds() // Use 'def' to declare the variable
-  def packerInit = new PackerInit()
-  def packerBuild = new PackerBuild()
+def call() {
+    def awsCred = new AwsCreds()
+    def packerInit = new PackerInit()
+    def packerBuild = new PackerBuild()
 
-  awsCred.call() 
-  packerInit.call()
-  packerBuild.call()
+    awsCred.awsCredentials()  // Call the correct method
+    packerInit.call()
+    packerBuild.call()
 }
