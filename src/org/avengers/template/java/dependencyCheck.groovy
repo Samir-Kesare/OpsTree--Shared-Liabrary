d406b3ecbcd26e331b47e42c577fadb72bc5b4b7
vyadavP7 <156056349+vyadavP7@users.noreply.github.com>
2024-02-18 00:12:27 +0530

    Update dependencyCheck.groovy

--- src/org/avengers/template/java/dependencyCheck.groovy
+++ src/org/avengers/template/java/dependencyCheck.groovy
@@ -1,1 +1,39 @@
+import org.avengers.common.GitCheckoutPrivate
+import org.avengers.common.cleanWorkspace
+import org.avengers.java.dependencyCheck.*
+import org.avengers.java.packageArtifacts.*
 
+def call(Map config = [:]){
+    def gitCheckout = new GitCheckoutPrivate()
+    def dpCheck = new dependencyCheck()
+    def cleanW = new cleanWorkspace()
+  
+    try {
+        // Clone repository 
+        gitCheckout.call(branch: config.branch, url: config.url)
+      
+        // Package artifacts
+        packageArtifacts.call()
+      
+        // perform Dependency Scanning 
+        dpCheck.call()
+      
+    } catch (e) {
+        echo 'DP check Failed !'
+
+        // clean workspace 
+        cleanW.call()
+  
+        throw e
+    } finally {
+        if (currentBuild.result == 'SUCCESS') {
+            echo 'DP check Successful!'
+
+            // archive HTML reports as artifacts
+            archiveArtifacts artifacts: '**/dependency-check-report.html'
+
+            // show workspace in tree structure
+            sh 'tree ${WORKSPACE}'
+        }
+    }
+}
