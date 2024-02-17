00f57304987a01dab01456a6ba7be8166878c134
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-02-17 15:20:25 +0530

    Update PackerInit.groovy

--- src/org/avengers/genericCi/packerAmi/PackerInit.groovy
+++ src/org/avengers/genericCi/packerAmi/PackerInit.groovy
@@ -1,9 +1,7 @@
 package org.avengers.genericCi.packerAmi
 
 def call() {
-    // Change to the directory containing the Packer configuration
-                    dir('/home/shreya/') {
-                        // Initialize Packer (if necessary)
-                        sh '/usr/bin/packer init .'
+    dir('/home/shreya/') {
+        sh '/usr/bin/packer init .'
     }
 }
