879dd7c8a5e6253ea698293bacc7901272202e8d
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-02-17 12:29:11 +0530

    Update AwsCreds.groovy

--- src/org/avengers/genericCi/packerAmi/AwsCreds.groovy
+++ src/org/avengers/genericCi/packerAmi/AwsCreds.groovy
@@ -1,1 +1,6 @@
+package org.avengers.genericCi.packerAmi
 
+def call() {
+     // Define AWS credentials
+    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'credentials', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']])
+    }
