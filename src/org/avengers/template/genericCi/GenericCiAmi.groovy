346f9ca6ea6a020ef4f333ec17856eff524b8906
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-02-17 12:30:32 +0530

    Update GenericCiAmi.groovy

--- src/org/avengers/template/genericCi/GenericCiAmi.groovy
+++ src/org/avengers/template/genericCi/GenericCiAmi.groovy
@@ -3,9 +3,11 @@ package org.avengers.template.genericCi
 import org.avengers.genericCi.packerAmi.*
 
 def call(){
+  awsCred = new AwsCreds ()
   packerInit = new PackerInit()
   packerBuild = new PackerBuild()
 
+  awsCreds .call()
   packerInit .call()
   packerBuild.call()
 }
