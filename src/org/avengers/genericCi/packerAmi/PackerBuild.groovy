7fdb3e0495e9b13798f63c0394e6e2771799a1fd
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-02-17 14:54:56 +0530

    Update PackerBuild.groovy

--- src/org/avengers/genericCi/packerAmi/PackerBuild.groovy
+++ src/org/avengers/genericCi/packerAmi/PackerBuild.groovy
@@ -6,7 +6,7 @@ def call() {
         // Initialize Packer (if necessary)
         withCredentials([[
             $class: 'AmazonWebServicesCredentialsBinding',
-            credentialsId: 'aws-credentials-id',
+            credentialsId: 'aws creds',
             accessKeyVariable: 'AWS_ACCESS_KEY_ID',
             secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
         ]]) {
