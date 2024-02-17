efbd83bfe3a6320f54acfefb44adc3c2c8f79374
vyadavP7 <156056349+vyadavP7@users.noreply.github.com>
2024-02-18 03:23:59 +0530

    Update packageArtifacts.groovy

--- src/org/avengers/common/packageArtifacts.groovy
+++ src/org/avengers/common/packageArtifacts.groovy
@@ -1,6 +1,12 @@
 package org.avengers.common
 
 def call() {
-    // package artifacts minus the testing
-    sh 'mvn clean package -DskipTests=true'
+    stage('Build') {
+        steps {
+            script {
+                // Package artifacts minus the testing
+                sh 'mvn clean package -DskipTests=true'
+            }
+        }
+    }
 }
