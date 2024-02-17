55ac51c8a46e5d27893cf142a6f5ba7a47e36ef4
vyadavP7 <156056349+vyadavP7@users.noreply.github.com>
2024-02-18 00:00:54 +0530

    Update dpCheck.groovy

--- src/org/avengers/java/dependencyCheck/dpCheck.groovy
+++ src/org/avengers/java/dependencyCheck/dpCheck.groovy
@@ -1,1 +1,6 @@
+package org.avengers.java.dependencyCheck
 
+def call() {
+    // Perform dependency check
+    dependencyCheck additionalArguments: '--scan target/ --format ALL', odcInstallation: 'DP-check'
+}
