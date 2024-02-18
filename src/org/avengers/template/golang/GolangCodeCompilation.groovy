c1e78830a06477ed753820d416d6ed86bb3cee8b
Khushi Malhotra <156056460+khushimalhoz@users.noreply.github.com>
2024-02-19 04:05:24 +0530

    Update GolangCodeCompilation.groovy

--- src/org/avengers/template/golang/GolangCodeCompilation.groovy
+++ src/org/avengers/template/golang/GolangCodeCompilation.groovy
@@ -1,33 +1,17 @@
-package org.mygurukulam.template
+package org.avengers.template.golang
 
-import org.mygurukulam.common.*
-import org.mygurukulam.Golang.*
+import org.avengers.common.*
+import org.avengers.golang.CodeCompilation.*
 
+def call(String url, String creds, String branch){
+  codecompilation = new CodeCompilation
+  gitCheckoutPrivate = new GitCheckoutPrivate()
+  linting = new Linting()
+  report = new Report()
 
-def call(String url, String cred, String branch, Map config = [:]) {
-
-    def cleanup = new org.mygurukulam.common.Cleanup()
-    def git = new org.mygurukulam.common.Checkout()
-    def credScan = new org.mygurukulam.common.CredScanningNopath()
-    // def compile = new org.mygurukulam.Golang.compilation()
-    def install  = new org.mygurukulam.Golang.Install()
-    def build  = new org.mygurukulam.Golang.Build()
-    //def analysis = new org.mygurukulam.Golang.StaticcodeAnalysis()
-    // def scanning = new org.mygurukulam.Golang.DependencyCheck()
-    // def test = new org.mygurukulam.Golang.UnitTest()
-    //def bug = new org.mygurukulam.Golang.BugAnalysis()
-    def ami = new org.mygurukulam.common.BuildAMIwithDir()
-
-    cleanup.call()
-    git.call(url, cred, branch)
-    credScan.call()
-    install.call()
-    build.call()
-    //bug.call()
-    // compile.call()
-    //analysis.call()
-    // scanning.call()
-    // test.call()
-    git.call(config.packerUrl, cred, branch)
-    ami.call(config.packerFile, config.directory)
+  installationPreRequisites.call()
+  gitCheckoutPrivate.call(url, creds, branch)
+  linting.call()
+  report.call()
 }
+  
