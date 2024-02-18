816f950025fffc642dac6270158d82f9b19f32bb
vikram445 <79625874+vikram445@users.noreply.github.com>
2024-02-19 01:56:20 +0530

    Update dependencies.groovy

--- src/org/avengers/python/staticCodeAnalysis/dependencies.groovy
+++ src/org/avengers/python/staticCodeAnalysis/dependencies.groovy
@@ -2,10 +2,10 @@ package org.avengers.python.staticCodeAnalysis
 
 def call() {
 stage('Install dependencies') {
-            steps {
+            
                 script {
                     sh 'python3 -m pip install -r requirements.txt'
                 }
             }
         } 
-}
+
