e64b5f9ae6a75aaa1195e2ec1dc10a509d9e8af3
vikram445 <79625874+vikram445@users.noreply.github.com>
2024-02-18 23:36:55 +0530

    Update archiveArtifacts.groovy

--- src/org/avengers/common/archiveArtifacts.groovy
+++ src/org/avengers/common/archiveArtifacts.groovy
@@ -1,4 +1,7 @@
-post {
+package org.avengers.common
+
+def call() {
+    post {
         always {
             // Archive pylint reports
             archiveArtifacts artifacts: '**/pylint.log', allowEmptyArchive: true
