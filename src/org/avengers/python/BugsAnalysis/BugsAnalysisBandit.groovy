45c1ea009995ebac4c93ea79c93b8b5e4b53aba6
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-02-18 11:30:39 +0530

    Update BugsAnalysisBandit.groovy

--- src/org/avengers/python/BugsAnalysis/BugsAnalysisBandit.groovy
+++ src/org/avengers/python/BugsAnalysis/BugsAnalysisBandit.groovy
@@ -2,15 +2,14 @@ package  org.avengers.python.BugsAnalysis
 def call() {
   stage('Bugs Analysis - Bandit') {
                 script {
-                    try {
+              
                         // Ensure Bandit is installed and run the analysis
-                        sh 'bandit --version' // Check Bandit version to ensure it's installed
+                        sh 'bandit --version' 
                         sh 'bandit -r . -f json -o bandit_report.json'
-                    } catch (Exception e) {
-                        echo "Bugs analysis failed: ${e.message}"
+                    
                     }
                 }
             }
-        }
+        
     
 
