25abd09ca61e4e39b6d9b87064a5b3ec9a7fcdf3
Khushi Malhotra <156056460+khushimalhoz@users.noreply.github.com>
2024-02-19 04:33:03 +0530

    Update GolangCodeCompilation.groovy

--- src/org/avengers/template/golang/GolangCodeCompilation.groovy
+++ src/org/avengers/template/golang/GolangCodeCompilation.groovy
@@ -5,10 +5,12 @@ import org.avengers.golang.CodeCompilation.*
 
 def call(String url, String creds, String branch){
   gitCheckoutPrivate = new GitCheckoutPrivate()
+  installationPreRequisites = new InstallationPreRequisites()
   codecompilation = new codecompilation()
 
   
   gitCheckoutPrivate.call(url, creds, branch)
+  installationPreRequisites.call()
   codecompilation.call()
 }
   
