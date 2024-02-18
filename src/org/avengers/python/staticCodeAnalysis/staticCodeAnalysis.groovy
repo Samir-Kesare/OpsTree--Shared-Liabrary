50a8948dcd573b250cf36e2f7e08ff9e7b2ca265
vikram445 <79625874+vikram445@users.noreply.github.com>
2024-02-19 01:55:55 +0530

    Update staticCodeAnalysis.groovy

--- src/org/avengers/python/staticCodeAnalysis/staticCodeAnalysis.groovy
+++ src/org/avengers/python/staticCodeAnalysis/staticCodeAnalysis.groovy
@@ -2,10 +2,10 @@ package org.avengers.python.staticCodeAnalysis
 
 def call() {
 stage('Static Code Analysis') {
-            steps {
+            
                 script {
                     sh 'pylint router/ client/ models/ utils/ app.py | tee pylint.log'
                 }
             }
         }
-    }
+    
