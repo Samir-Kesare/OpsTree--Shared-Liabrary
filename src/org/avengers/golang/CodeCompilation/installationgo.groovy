fc7064dcdd8d06494a00f5a159ddd5be83cb2c7e
Khushi Malhotra <156056460+khushimalhoz@users.noreply.github.com>
2024-02-19 04:47:56 +0530

    Update installationgo.groovy

--- src/org/avengers/golang/CodeCompilation/installationgo.groovy
+++ src/org/avengers/golang/CodeCompilation/installationgo.groovy
@@ -4,7 +4,7 @@ def call() {
     stage('Installation Pre-Requisites') {
         script {
             // Update apt packages
-            sudo -S apt update
+            sh 'sudo -S apt update'
             // Install Go using snap
             sh 'sudo snap install go --classic'
           
