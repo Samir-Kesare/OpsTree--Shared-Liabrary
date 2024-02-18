8e841632a878e6af9eb7f66645e8dd3ee25f7c2c
vikram445 <79625874+vikram445@users.noreply.github.com>
2024-02-19 02:23:58 +0530

    Update PythonStaticCodeAnalysis.groovy

--- src/org/avengers/template/python/PythonStaticCodeAnalysis.groovy
+++ src/org/avengers/template/python/PythonStaticCodeAnalysis.groovy
@@ -5,11 +5,11 @@ import org.avengers.python.staticCodeAnalysis.*
 
 def call(String url, String creds, String branch) {
   
-     gitCheckoutPrivate = new GitCheckoutPrivate()
-     virtual = new VirtualEnv()
-     dep = new Dependencies()
-     code = new StaticCodeAnalysis()
-     arch = new ArchiveArtifacts() 
+    gitCheckoutPrivate = new GitCheckoutPrivate()
+    virtual = new virtualEnv()
+    dep = new dependencies()
+    code = new staticCodeAnalysis()
+    arch = new ArchiveArtifacts() 
   
     gitCheckoutPrivate.call(url, creds, branch)
     virtual.call()
