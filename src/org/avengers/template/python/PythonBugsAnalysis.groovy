13dab8f353cf30df064180fce3c519988732e370
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-02-18 10:16:35 +0530

    Update PythonBugsAnalysis.groovy

--- src/org/avengers/template/python/PythonBugsAnalysis.groovy
+++ src/org/avengers/template/python/PythonBugsAnalysis.groovy
@@ -1,1 +1,16 @@
+package org.avengers.template.python
 
+import org.avengers.common.*
+import org.avengers.python.BugsAnalysis.*
+
+def call(String url, String creds, String branch){
+  gitCheckoutPrivate = new GitCheckoutPrivate()
+  cleanWorkspace = new cleanWorkspace()
+  bugsAnalysis-Bandit = new BugsAnalysis-Bandit()
+  installDependencies = new InstallDependencies()
+
+  gitCheckoutPrivate.call(url, creds, branch)
+  cleanWorkspace.call()
+  bugsAnalysis-Bandit.call()
+  installDependencies.call()
+}
