a37e99024ffaa6312f294aede753705bb1b46756
vikram445 <79625874+vikram445@users.noreply.github.com>
2024-02-19 01:14:22 +0530

    Update PythonStaticCodeAnalysis.groovy

--- src/org/avengers/template/python/PythonStaticCodeAnalysis.groovy
+++ src/org/avengers/template/python/PythonStaticCodeAnalysis.groovy
@@ -1,19 +1,21 @@
 package org.avengers.template.python
 
 import org.avengers.common.*
-import org.avengers.python.staticcodeanalysis.*
+import org.avengers.python.staticCodeAnalysis.*
 
-def call(String url, String creds, String branch, String depVersion, String javaVersion){
-  javaDownload = new JavaDownload()
-  downloadDependencyCheck = new DownloadDependencyCheck()
+def call(String url, String creds, String branch){
+  
   gitCheckoutPrivate = new GitCheckoutPrivate()
-  dependencyCheck = new DependencyCheck()
-  clean = new Clean()
-
-  javaDownload.call(javaVersion)
-  downloadDependencyCheck.call(depVersion) 
+  virtual = new virtualEnv()
+  dep = new dependencies()
+  code = new staticCodeAnalysis()
+  arch = new ArchiveArtifacts() 
+  
   gitCheckoutPrivate.call(url, creds, branch)
-  dependencyCheck.call()
-  clean.call()
+  virtual.call()
+  dep.call()
+  code.call()
+  arch.call()
+  
 }
  
