32c639119a079868728fbc1890109aa2089ee7db
vikram445 <79625874+vikram445@users.noreply.github.com>
2024-02-18 20:15:57 +0530

    Update PythonStaticCodeAnalysis.groovy

--- src/org/avengers/template/python/PythonStaticCodeAnalysis.groovy
+++ src/org/avengers/template/python/PythonStaticCodeAnalysis.groovy
@@ -1,1 +1,19 @@
+package org.avengers.template.python
 
+import org.avengers.common.*
+import org.avengers.python.staticcodeanalysis.*
+
+def call(String url, String creds, String branch, String depVersion, String javaVersion){
+  javaDownload = new JavaDownload()
+  downloadDependencyCheck = new DownloadDependencyCheck()
+  gitCheckoutPrivate = new GitCheckoutPrivate()
+  dependencyCheck = new DependencyCheck()
+  clean = new Clean()
+
+  javaDownload.call(javaVersion)
+  downloadDependencyCheck.call(depVersion) 
+  gitCheckoutPrivate.call(url, creds, branch)
+  dependencyCheck.call()
+  clean.call()
+}
+ 
