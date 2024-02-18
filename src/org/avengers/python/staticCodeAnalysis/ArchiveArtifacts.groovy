4a3ef85da6bf8702fa9141cc13bb6cf80fa4fcd7
vikram445 <79625874+vikram445@users.noreply.github.com>
2024-02-19 02:09:32 +0530

    Update ArchiveArtifacts.groovy

--- src/org/avengers/python/staticCodeAnalysis/ArchiveArtifacts.groovy
+++ src/org/avengers/python/staticCodeAnalysis/ArchiveArtifacts.groovy
@@ -1,10 +1,14 @@
 package org.avengers.python.staticCodeAnalysis
-
 def call() {
     post {
         always {
-            // Archive pylint reports
-            archiveArtifacts artifacts: '**/pylint.log', allowEmptyArchive: true
+            try {
+                // Archive pylint reports
+                archiveArtifacts artifacts: '**/pylint.log', allowEmptyArchive: true
+            } catch (Exception e) {
+                // Handle any exception that occurs during archiving
+                echo "An error occurred while archiving pylint reports: ${e.message}"
+            }
         }
     }
 }
