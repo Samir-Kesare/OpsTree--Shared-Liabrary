83d8f6b42f4022dad321ad549c1d7e195556f0af
aakashtripathi-snaatak <156056344+aakashtripathi-snaatak@users.noreply.github.com>
2024-02-19 15:45:14 +0530

    Update LicenseScan.groovy

--- src/org/avengers/template/genericCi/LicenseScan.groovy
+++ src/org/avengers/template/genericCi/LicenseScan.groovy
@@ -7,37 +7,16 @@ def call(Map config = [:]){
   gitClone = new gitCheckout()
   fossa = new Fossa()
   scan = new Scan()
-  cleanAfterArchive = new package org.avengers.template.genericCi
-
-import org.avengers.common.*
-import org.avengers.credScanning.*
-
-def call(Map config = [:], String gitLeaksVersion, String reportName){
-  gitClone = new gitCheckout()
-  gitLeaks = new GitLeaks()
-  scan = new Scan()
-  cleanAfterArchive = new CleanAfterArchive()
+  cleanworkspace = new CleanWorkSpace()
 
   try {
     gitClone.call(branch: config.branch, url: config.url  )
-    gitLeaks.call(gitLeaksVersion)
-    scan.call(reportName)
+    fossa.call()
+    scan.call()
     } catch (Exception e) {
-        echo "Check your code for credential Leak: ${e.message}"
+        echo "Check your code for License Issues: ${e.message}"
     } finally {
-       cleanAfterArchive.call(reportName) 
-    }
-  
-}()
-
-  try {
-    gitClone.call(branch: config.branch, url: config.url  )
-    gitLeaks.call(gitLeaksVersion)
-    scan.call(reportName)
-    } catch (Exception e) {
-        echo "Check your code for credential Leak: ${e.message}"
-    } finally {
-       cleanAfterArchive.call(reportName) 
+       cleanworkspace.call() 
     }
   
 }
