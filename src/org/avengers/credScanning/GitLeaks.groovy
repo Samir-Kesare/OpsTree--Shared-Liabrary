3dfa69b43171acc2e2e499dfca717a4b94bbc280
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-19 14:13:56 +0530

    Update GitLeaks.groovy

--- src/org/avengers/credScanning/GitLeaks.groovy
+++ src/org/avengers/credScanning/GitLeaks.groovy
@@ -1,5 +1,12 @@
 package org.avengers.credScanning
 
 def call(String gitLeaksVersion){
+  stage('Download and Install Gitleaks') {
+        script {
+           sh "wget https://github.com/gitleaks/gitleaks/releases/download/v${gitLeaksVersion}/gitleaks_${gitLeaksVersion}_linux_x64.tar.gz"
+                  // Extract Gitleaks
+            sh "tar xvzf gitleaks_${gitLeaksVersion}_linux_x64.tar.gz"
+        }
+    }
   
 }
