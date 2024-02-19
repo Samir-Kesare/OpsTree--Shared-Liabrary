84d204ec9d7c10dd07b1a6b534aec716f96a4a4b
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-02-19 12:41:06 +0530

    Update salaryCI.groovy

--- src/org/avengers/template/salary/salaryCI.groovy
+++ src/org/avengers/template/salary/salaryCI.groovy
@@ -3,3 +3,15 @@ package org.avengers.template
 import org.avengers.common.gitCheckout
 import org.avengers.common.cleanWorkspace
 import org.avengers.java.*
+ 
+def call(Map config = [:]){
+    def gitCheckout = new gitCheckout()
+    def javaCompile = new compile()
+    // def staticCodeAnalysis = new staticCodeAnalysis()
+    def cleanWorkspace = new cleanWorkspace()
+
+    gitCheckout.call(branch: config.branch, url: config.url  )
+    javaCompile.call()
+    // staticCodeAnalysis.call()
+    cleanWorkspace.call()
+}
