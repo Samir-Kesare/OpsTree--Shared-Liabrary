6de0f4e74903d172f5bc3d83d806eccb84a08366
Khushi Malhotra <156056460+khushimalhoz@users.noreply.github.com>
2024-02-19 19:57:26 +0530

    Update CommitSignOffPipeline.groovy

--- src/org/avengers/template/CommitSignOff/CommitSignOffPipeline.groovy
+++ src/org/avengers/template/CommitSignOff/CommitSignOffPipeline.groovy
@@ -1,6 +1,6 @@
 package org.avengers.template.CommitSignOff
 
-import org.avengers.CommitSignOff.GitCommitSignOff
+import org.avengers.CommitSignOff.*
 
 def call(){
     def gitCommitSignOff = new GitCommitSignOff()
