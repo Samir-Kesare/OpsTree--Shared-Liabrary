8eccd195b44c38fef29d31b690574a6391b69b0d
Khushi Malhotra <156056460+khushimalhoz@users.noreply.github.com>
2024-02-19 19:22:14 +0530

    Update CommitSignOffPipeline.groovy

--- src/org/avengers/template/CommitSignOff/CommitSignOffPipeline.groovy
+++ src/org/avengers/template/CommitSignOff/CommitSignOffPipeline.groovy
@@ -1,26 +1,16 @@
 package org.avengers.template.CommitSignOff
 
-import org.avengers.common.GitCheckoutPrivate 
+import org.avengers.common.*
+import org.avengers.CommitSignOff.*
+    def call(String url, String creds, String branch){
+        gitCheckoutPrivate = new GitCheckoutPrivate()
+        gitcommitsignoff = new GitCommitSignOff()
 
-class CommitSignOffPipeline {
-    static void performCommitSignOff() {
-        node {
-            stage('Fetch Last Commit') {
-                checkout scm
-                
-                def gitCommit = sh(script: 'git log -1 --pretty=%an', returnStdout: true).trim()
-                def gitCommitMsg = sh(script: 'git log -1 --pretty=%B', returnStdout: true).trim()
-                
-                
-                if (gitCommitMsg.contains('Signed-off-by:')) {
-                    echo "Last commit by ${gitCommit} has a sign-off."
-                } else {
-                    GitCheckoutPrivate gitCheckoutPrivate = new GitCheckoutPrivate() 
-                    gitCheckoutPrivate.call(url, creds, branch) 
-                }
-            }
-        }
+         gitCheckoutPrivate.call()
+        gitcommitsignoff.call()
     }
-}
+
+
+
 
 
