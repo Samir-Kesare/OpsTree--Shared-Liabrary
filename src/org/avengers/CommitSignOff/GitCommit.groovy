0cd443a37f3a8fce20d6e2f8ccdd0759879f4710
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-20 00:59:13 +0530

    Update GitCommit.groovy

--- src/org/avengers/CommitSignOff/GitCommit.groovy
+++ src/org/avengers/CommitSignOff/GitCommit.groovy
@@ -13,42 +13,38 @@ def call() {
                 } else {
                     // Check if there are changes to commit
                     try {
-                        def changes = sh(script: 'git diff --exit-code', returnStatus: true).returnStatus
-                        if (changes == 0) {
-                            // No changes to commit
-                            echo "No changes to commit."
-                        } else {
-                            // Add and commit changes
-                            sh 'git add .'
-                            sh "git commit -m 'Auto-commit before sign-off'"
-                            
-                            // Iterate through usernames and find matching email and name
-                            def usernameEmailMap = [
-                                'vikram445': 'vikram.bisht@opstree.com',
-                                'aakashtripathi-snaatak': 'aakash.tripathi.snaatak@mygurukulam.co',
-                                'vyadavP7': 'vidhi.yadhav.snaatak@mygurukulam.co',
-                                'code-shantanu': 'shantanu.chauhan.snaatak@mygurukulam.co',
-                                'Vishalkk1998': 'vishal.kesarwani.snaatak@mygurukulam.co',
-                                'Panu-S-Harshit-Ninja-07': 'harshit.singh.snaatak@mygurukulam.co',
-                                'khushimalhoz': 'khushi.malhotra.snaatak@mygurukulam.co',
-                                'Snatak-SamirKesare': 'samir.kesare.snaatak@mygurukulam.co',
-                                'Parasharam-Desai': 'parasharam.desai.snaatak@mygurukulam.co',
-                                'tripathishikha1': 'shikha.tripathi.snaatak@mygurukulam.co',
-                                'shreya-snaatak': 'shikha.tripathi.snaatak@mygurukulam.co',
-                                'Nidhi-bhardwaj123': 'nidhi.bhardwaj.snaatak@mygurukulam.co'
-                            ]   
-                            def email = usernameEmailMap[gitCommit]           
-                            if (email) {
-                                def newCommitMsg = "${gitCommitMsg}\nSigned-off-by: ${email}"
-                                sh "git commit -am '${newCommitMsg}'"
-                                echo "New commit message updated with sign-off by ${gitCommit}."
-                            } else {
-                                error "Unable to find email for ${gitCommit}."
-                            }
-                        }
+                        sh 'git diff --exit-code'
+                        // No exception means there are no changes
+                        echo "No changes to commit."
                     } catch (Exception e) {
-                        echo "Error occurred while checking for changes: ${e.message}"
-                        error "Failed to check for changes."
+                        // Exception indicates there are changes
+                        // Add and commit changes
+                        sh 'git add .'
+                        sh "git commit -m 'Auto-commit before sign-off'"
+                        
+                        // Iterate through usernames and find matching email and name
+                        def usernameEmailMap = [
+                            'vikram445': 'vikram.bisht@opstree.com',
+                            'aakashtripathi-snaatak': 'aakash.tripathi.snaatak@mygurukulam.co',
+                            'vyadavP7': 'vidhi.yadhav.snaatak@mygurukulam.co',
+                            'code-shantanu': 'shantanu.chauhan.snaatak@mygurukulam.co',
+                            'Vishalkk1998': 'vishal.kesarwani.snaatak@mygurukulam.co',
+                            'Panu-S-Harshit-Ninja-07': 'harshit.singh.snaatak@mygurukulam.co',
+                            'khushimalhoz': 'khushi.malhotra.snaatak@mygurukulam.co',
+                            'Snatak-SamirKesare': 'samir.kesare.snaatak@mygurukulam.co',
+                            'Parasharam-Desai': 'parasharam.desai.snaatak@mygurukulam.co',
+                            'tripathishikha1': 'shikha.tripathi.snaatak@mygurukulam.co',
+                            'shreya-snaatak': 'shikha.tripathi.snaatak@mygurukulam.co',
+                            'Nidhi-bhardwaj123': 'nidhi.bhardwaj.snaatak@mygurukulam.co'
+                        ]   
+                        def email = usernameEmailMap[gitCommit]           
+                        if (email) {
+                            def newCommitMsg = "${gitCommitMsg}\nSigned-off-by: ${email}"
+                            sh "git commit -am '${newCommitMsg}'"
+                            echo "New commit message updated with sign-off by ${gitCommit}."
+                        } else {
+                            error "Unable to find email for ${gitCommit}."
+                        }
                     }
                 }
             }
