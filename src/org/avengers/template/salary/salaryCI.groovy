15b3bb30dd70ce4a34d21770b91ed1c311d92518
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-02-19 12:54:46 +0530

    Update salaryCI.groovy

--- src/org/avengers/template/salary/salaryCI.groovy
+++ src/org/avengers/template/salary/salaryCI.groovy
@@ -3,15 +3,28 @@ package org.avengers.template
 import org.avengers.common.gitCheckout
 import org.avengers.common.cleanWorkspace
 import org.avengers.java.compile.*
- 
+import org.avengers.java.staticCodeAnalysis.*
+
 def call(Map config = [:]){
     def gitCheckout = new gitCheckout()
     def javaCompile = new compile()
-    // def staticCodeAnalysis = new staticCodeAnalysis()
+    def staticCodeAnalysis = new staticCodeAnalysis()
     def cleanWorkspace = new cleanWorkspace()
-
+    try{
     gitCheckout.call(branch: config.branch, url: config.url  )
     javaCompile.call()
-    // staticCodeAnalysis.call()
-    cleanWorkspace.call()
+    staticCodeAnalysis.call()
+    }
+    catch (e){
+        echo 'Analysis Failed'
+        cleanWorkspace.call()
+        throw e
+    }
+    finally {
+         def currentResult = currentBuild.result ?: 'SUCCESS'
+        if ((currentResult == 'UNSTABLE')||(currentResult == 'ABORTED')) {
+        cleanWorkspace.call()
+            // echo 'This will run only if the run was marked as unstable'
+        }
+    }
 }
