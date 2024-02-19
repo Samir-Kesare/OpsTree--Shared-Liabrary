699c16e94f4679e4f4e413acbac934eeb197441f
aakashtripathi-snaatak <156056344+aakashtripathi-snaatak@users.noreply.github.com>
2024-02-19 15:56:57 +0530

    Update Fossa.groovy

--- src/org/avengers/licenseScanning/Fossa.groovy
+++ src/org/avengers/licenseScanning/Fossa.groovy
@@ -3,8 +3,9 @@ package org.avengers.licenseScanning
 def call() {
     stage('Download and Install FOSSA') {
         script {
-            env.FOSSA_API_KEY = credentials('fossaToken')
+             withCredentials([string(credentialsId: 'fossaToken', variable: 'FOSSA_API_KEY')]){
             sh 'curl -H \'Cache-Control: no-cache\' https://raw.githubusercontent.com/fossas/fossa-cli/master/install-latest.sh | bash'
+             }
         }
     }
 }
