a00a60421f7430a107e30127c3c0a0734feeeb4d
Khushi Malhotra <156056460+khushimalhoz@users.noreply.github.com>
2024-02-19 19:44:12 +0530

    Update CommitSignOffPipeline.groovy

--- src/org/avengers/template/CommitSignOff/CommitSignOffPipeline.groovy
+++ src/org/avengers/template/CommitSignOff/CommitSignOffPipeline.groovy
@@ -4,8 +4,6 @@ import org.avengers.CommitSignOff.GitCommitSignOff
 
 def call(String url, String creds, String branch){
     def gitCheckoutPrivate = new GitCheckoutPrivate()
-    def gitCommitSignOff = new GitCommitSignOff()
 
-    gitCheckoutPrivate.call() 
     gitCommitSignOff.signOffCommit("gitCommitPlaceholder", "gitCommitMsgPlaceholder")
 }
