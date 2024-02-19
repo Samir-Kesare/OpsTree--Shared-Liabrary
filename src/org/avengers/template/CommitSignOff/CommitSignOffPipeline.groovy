183456b7c4e90095ec9ba3e30c3bc87d7239cf67
Khushi Malhotra <156056460+khushimalhoz@users.noreply.github.com>
2024-02-19 17:19:14 +0530

    Create CommitSignOffPipeline.groovy

---
+++ src/org/avengers/template/CommitSignOff/CommitSignOffPipeline.groovy
@@ -0,0 +1,23 @@
+package org.avengers.template
+
+import org.avengers.CommitSignOff.GitCommitSignOff
+
+class CommitSignOffPipeline {
+    static void performCommitSignOff() {
+        node {
+            stage('Fetch Last Commit') {
+                checkout scm
+                
+                def gitCommit = sh(script: 'git log -1 --pretty=%an', returnStdout: true).trim()
+                def gitCommitMsg = sh(script: 'git log -1 --pretty=%B', returnStdout: true).trim()
+                
+                // Check if commit sign-off is present
+                if (gitCommitMsg.contains('Signed-off-by:')) {
+                    echo "Last commit by ${gitCommit} has a sign-off."
+                } else {
+                    GitCommitSignOff.signOffCommit(gitCommit, gitCommitMsg)
+                }
+            }
+        }
+    }
+}
