2e311ef1019b37b7f40a2caad73a0b7b3bcfa147
Khushi Malhotra <156056460+khushimalhoz@users.noreply.github.com>
2024-02-19 10:30:38 +0530

    Update GolangCodeCompilation.groovy

--- src/org/avengers/template/golang/GolangCodeCompilation.groovy
+++ src/org/avengers/template/golang/GolangCodeCompilation.groovy
@@ -1,17 +1,38 @@
-package org.avengers.template.golang
+#package org.avengers.template.golang
 
+
+#import org.avengers.common.*
+#import org.avengers.golang.CodeCompilation.*
+
+#def call(String url, String creds, String branch){
+  #gitCheckoutPrivate = new GitCheckoutPrivate()
+  #installationgo = new installationgo()
+  #codecompilation = new codecompilation()
+
+  
+  #gitCheckoutPrivate.call(url, creds, branch)
+  #installationgo.call()
+  #codecompilation.call()
+#}
+  
+package org.avengers.template.golang
 
 import org.avengers.common.*
 import org.avengers.golang.CodeCompilation.*
 
-def call(String url, String creds, String branch){
-  gitCheckoutPrivate = new GitCheckoutPrivate()
-  installationgo = new installationgo()
-  codecompilation = new codecompilation()
+def call(String url, String creds, String branch) {
+    def gitCheckoutPrivate = new GitCheckoutPrivate()
+    def installationgo = new InstallationGo()
+    def codeCompilation = new CodeCompilation()
 
-  
-  gitCheckoutPrivate.call(url, creds, branch)
-  installationgo.call()
-  codecompilation.call()
+    try {
+        gitCheckoutPrivate.call(url, creds, branch)
+        installationgo.call()
+        codeCompilation.call()
+    } catch (Exception e) {
+        // Handle exceptions gracefully, e.g., by failing the pipeline with an error message
+        echo "Error occurred: ${e.message}"
+        currentBuild.result = 'FAILURE'
+        throw e
+    }
 }
-  
