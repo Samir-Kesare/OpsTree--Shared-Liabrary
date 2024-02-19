f505c6782b9e2734419ec0686ce7ef5ced156361
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-19 14:15:59 +0530

    Create Scan.groovy

---
+++ src/org/avengers/credScanning/Scan.groovy
@@ -0,0 +1,10 @@
+package org.avengers.credScanning
+
+def call(String reportName){
+  stage('Gitleaks Scan') {
+        script {
+           sh "./gitleaks detect -r ${reportName}"
+        }
+    }
+  
+}
