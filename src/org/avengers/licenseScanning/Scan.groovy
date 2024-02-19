60017bc9ffe38d578abb492427443969e3dd4276
aakashtripathi-snaatak <156056344+aakashtripathi-snaatak@users.noreply.github.com>
2024-02-19 15:38:59 +0530

    Update Scan.groovy

--- src/org/avengers/licenseScanning/Scan.groovy
+++ src/org/avengers/licenseScanning/Scan.groovy
@@ -1,1 +1,10 @@
+package org.avengers.licenseScanning
 
+def call() {
+    stage('FOSSA Analyze & Test') {
+        script {
+            sh 'fossa analyze'
+            sh 'fossa test'
+        }
+    }
+}
