f9779ce7c5f546af9a8aebbead388fd13bdb69b8
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-20 00:26:43 +0530

    Update GitCommit.groovy

--- src/org/avengers/CommitSignOff/GitCommit.groovy
+++ src/org/avengers/CommitSignOff/GitCommit.groovy
@@ -11,30 +11,37 @@ def call() {
                 if (gitCommitMsg.contains('Signed-off-by:')) {
                     echo "Last commit by ${gitCommit} has a sign-off."
                 } else {
-                    // Iterate through usernames and find matching email and name
-                    def usernameEmailMap = [
-                        'vikram445': 'vikram.bisht@opstree.com',
-                        'aakashtripathi-snaatak': 'aakash.tripathi.snaatak@mygurukulam.co',
-                        'vyadavP7': 'vidhi.yadhav.snaatak@mygurukulam.co',
-                        'code-shantanu': 'shantanu.chauhan.snaatak@mygurukulam.co',
-                        'Vishalkk1998': 'vishal.kesarwani.snaatak@mygurukulam.co',
-                        'Panu-S-Harshit-Ninja-07': 'harshit.singh.snaatak@mygurukulam.co',
-                        'khushimalhoz': 'khushi.malhotra.snaatak@mygurukulam.co',
-                        'Snatak-SamirKesare': 'samir.kesare.snaatak@mygurukulam.co',
-                        'Parasharam-Desai': 'parasharam.desai.snaatak@mygurukulam.co',
-                        'tripathishikha1': 'shikha.tripathi.snaatak@mygurukulam.co',
-                        'shreya-snaatak': 'shikha.tripathi.snaatak@mygurukulam.co',
-                        'Nidhi-bhardwaj123': 'nidhi.bhardwaj.snaatak@mygurukulam.co'
-                    ]   
-                    def email = usernameEmailMap[gitCommit]           
-                    if (email) {
-                        def newCommitMsg = "${gitCommitMsg}\nSigned-off-by: ${email}"
-                        sh "git commit -am '${newCommitMsg}'"
-                        echo "New commit message updated with sign-off by ${gitCommit}."
+                    // Check if there are changes to commit
+                    def hasChanges = sh(script: 'git status --short', returnStdout: true).trim()
+                    if (hasChanges) {
+                        // Iterate through usernames and find matching email and name
+                        def usernameEmailMap = [
+                            'vikram445': 'vikram.bisht@opstree.com',
+                            'aakashtripathi-snaatak': 'aakash.tripathi.snaatak@mygurukulam.co',
+                            'vyadavP7': 'vidhi.yadhav.snaatak@mygurukulam.co',
+                            'code-shantanu': 'shantanu.chauhan.snaatak@mygurukulam.co',
+                            'Vishalkk1998': 'vishal.kesarwani.snaatak@mygurukulam.co',
+                            'Panu-S-Harshit-Ninja-07': 'harshit.singh.snaatak@mygurukulam.co',
+                            'khushimalhoz': 'khushi.malhotra.snaatak@mygurukulam.co',
+                            'Snatak-SamirKesare': 'samir.kesare.snaatak@mygurukulam.co',
+                            'Parasharam-Desai': 'parasharam.desai.snaatak@mygurukulam.co',
+                            'tripathishikha1': 'shikha.tripathi.snaatak@mygurukulam.co',
+                            'shreya-snaatak': 'shikha.tripathi.snaatak@mygurukulam.co',
+                            'Nidhi-bhardwaj123': 'nidhi.bhardwaj.snaatak@mygurukulam.co'
+                        ]   
+                        def email = usernameEmailMap[gitCommit]           
+                        if (email) {
+                            def newCommitMsg = "${gitCommitMsg}\nSigned-off-by: ${email}"
+                            sh "git commit -am '${newCommitMsg}'"
+                            echo "New commit message updated with sign-off by ${gitCommit}."
+                        } else {
+                            error "Unable to find email for ${gitCommit}."
+                        }
                     } else {
-                        error "Unable to find email for ${gitCommit}."
+                        echo "No changes to commit."
                     }
                 }
             }
         }
     }
+
