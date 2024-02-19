abb617bed4fa1bf9a78e63be9a100fa0ec806f6c
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-20 00:21:54 +0530

    Update GitCommit.groovy

--- src/org/avengers/CommitSignOff/GitCommit.groovy
+++ src/org/avengers/CommitSignOff/GitCommit.groovy
@@ -2,38 +2,44 @@ package org.avengers.CommitSignOff
 
 def call() {
     stage('Fetch Last Commit') {
-        checkout scm
-        script {
-            def gitCommit = sh(script: 'git log -1 --pretty=%an', returnStdout: true).trim()
-            def gitCommitMsg = sh(script: 'git log -1 --pretty=%B', returnStdout: true).trim()
-                    
-            // Check if commit sign-off is present
-            if (gitCommitMsg.contains('Signed-off-by:')) {
-                echo "Last commit by ${gitCommit} has a sign-off."
-            } else {
-                // Iterate through usernames and find matching email and name
-                def usernameEmailMap = [
-                    'vikram445': 'vikram.bisht@opstree.com',
-                    'aakashtripathi-snaatak': 'aakash.tripathi.snaatak@mygurukulam.co',
-                    'vyadavP7': 'vidhi.yadhav.snaatak@mygurukulam.co',
-                    'code-shantanu': 'shantanu.chauhan.snaatak@mygurukulam.co',
-                    'Vishalkk1998': 'vishal.kesarwani.snaatak@mygurukulam.co',
-                    'Panu-S-Harshit-Ninja-07': 'harshit.singh.snaatak@mygurukulam.co',
-                    'khushimalhoz': 'khushi.malhotra.snaatak@mygurukulam.co',
-                    'Snatak-SamirKesare': 'samir.kesare.snaatak@mygurukulam.co',
-                    'Parasharam-Desai': 'parasharam.desai.snaatak@mygurukulam.co',
-                    'tripathishikha1': 'shikha.tripathi.snaatak@mygurukulam.co',
-                    'shreya-snaatak': 'shikha.tripathi.snaatak@mygurukulam.co',
-                    'Nidhi-bhardwaj123': 'nidhi.bhardwaj.snaatak@mygurukulam.co'
-                ]   
-                def email = usernameEmailMap[gitCommit]           
-                    if (email) {
-                        sh "git commit --amend --signoff --author='${gitCommit} <${email}>' -m '${gitCommitMsg} Signed-off-by: ${email}'"
-                        echo "Commit message updated with sign-off by ${gitCommit}."
+            checkout scm // This line will check out the source code from the configured Git repository
+            script {
+                def gitCommit = sh(script: 'git log -1 --pretty=%an', returnStdout: true).trim()
+                def gitCommitMsg = sh(script: 'git log -1 --pretty=%B', returnStdout: true).trim()
+                
+                // Check if commit sign-off is present
+                if (gitCommitMsg.contains('Signed-off-by:')) {
+                    echo "Last commit by ${gitCommit} has a sign-off."
+                } else {
+                    // Check if there are changes to commit
+                    def hasChanges = sh(script: 'git diff --exit-code', returnStatus: true).exitStatus == 1
+                    if (hasChanges) {
+                        // Iterate through usernames and find matching email and name
+                        def usernameEmailMap = [
+                            'vikram445': 'vikram.bisht@opstree.com',
+                            'aakashtripathi-snaatak': 'aakash.tripathi.snaatak@mygurukulam.co',
+                            'vyadavP7': 'vidhi.yadhav.snaatak@mygurukulam.co',
+                            'code-shantanu': 'shantanu.chauhan.snaatak@mygurukulam.co',
+                            'Vishalkk1998': 'vishal.kesarwani.snaatak@mygurukulam.co',
+                            'Panu-S-Harshit-Ninja-07': 'harshit.singh.snaatak@mygurukulam.co',
+                            'khushimalhoz': 'khushi.malhotra.snaatak@mygurukulam.co',
+                            'Snatak-SamirKesare': 'samir.kesare.snaatak@mygurukulam.co',
+                            'Parasharam-Desai': 'parasharam.desai.snaatak@mygurukulam.co',
+                            'tripathishikha1': 'shikha.tripathi.snaatak@mygurukulam.co',
+                            'shreya-snaatak': 'shikha.tripathi.snaatak@mygurukulam.co',
+                            'Nidhi-bhardwaj123': 'nidhi.bhardwaj.snaatak@mygurukulam.co'
+                        ]   
+                        def email = usernameEmailMap[gitCommit]           
+                        if (email) {
+                            sh "git commit --amend --signoff --author='${gitCommit} <${email}>' -m '${gitCommitMsg} Signed-off-by: ${email}'"
+                            echo "Commit message updated with sign-off by ${gitCommit}."
+                        } else {
+                            error "Unable to find email for ${gitCommit}."
+                        }
                     } else {
-                        error "Unable to find email for ${gitCommit}."
+                        echo "No changes to commit."
                     }
                 }
+            }
         }
     }
-}
