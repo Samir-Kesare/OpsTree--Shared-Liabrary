d0f56bac3b333cabbbe947e60bac86a716026cfb
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-02-21 01:35:43 +0530

    Create licenceScan.groovy

---
+++ src/org/avengers/licenseScanning/licenceScan.groovy
@@ -0,0 +1,18 @@
+package org.avengers.licenseScanning
+
+def installFossa() {
+    stage('Download and Install FOSSA') {
+        script {           
+            sh 'curl -H \'Cache-Control: no-cache\' https://raw.githubusercontent.com/fossas/fossa-cli/master/install-latest.sh | bash'
+        }
+    }
+}
+
+def scan() {
+    stage('FOSSA Analyze & Test') {
+        script {
+            sh 'fossa analyze'
+            sh 'fossa test'
+        }
+    }
+}
