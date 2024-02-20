b9a174ba457c91f91f16207eba799f0d2c1fdc3c
vyadavP7 <156056349+vyadavP7@users.noreply.github.com>
2024-02-20 21:59:16 +0530

    Update javaDast.groovy

--- src/org/avengers/template/java/javaDast.groovy
+++ src/org/avengers/template/java/javaDast.groovy
@@ -7,11 +7,12 @@ def call(String url, String creds, String branch, String zapVersion, String curr
   gitCheckoutPrivate = new GitCheckoutPrivate()
   owaspZapInstallation = new OwaspZapInstallation()
   runZap = new RunZap()
+  def cleanW = new cleanWorkspace()
 
   gitCheckoutPrivate.call(url, creds, branch)
   owaspZapInstallation.call(zapVersion)
   runZap.call(zapVersion, currentWorkspace)
-
+  cleanW.call()
   // Archive ZAP scan results
   archiveArtifacts artifacts: '**/results.html', allowEmptyArchive: true
 }
