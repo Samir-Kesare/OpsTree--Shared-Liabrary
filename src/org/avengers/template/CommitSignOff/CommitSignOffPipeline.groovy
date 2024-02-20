791d10fad716c25d787ca7d89302a1b36bef05a7
Khushi Malhotra <156056460+khushimalhoz@users.noreply.github.com>
2024-02-20 22:00:57 +0530

    Update CommitSignOffPipeline.groovy

--- src/org/avengers/template/CommitSignOff/CommitSignOffPipeline.groovy
+++ src/org/avengers/template/CommitSignOff/CommitSignOffPipeline.groovy
@@ -5,8 +5,8 @@ import org.avengers.common.*
 
 def call(){
     gitCommit = new GitCommit()
-    gitCheckoutPrivate = new GitCheckoutPrivate()
+   
 
     gitCommit.call()
-    gitCheckoutPrivate.call(url, creds, branch)
+   
 }
