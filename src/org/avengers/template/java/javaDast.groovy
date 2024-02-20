2c03cf8ebd8b53bcd8174562d792db921f5ac2fc
vyadavP7 <156056349+vyadavP7@users.noreply.github.com>
2024-02-20 15:50:08 +0530

    Update javaDast.groovy

--- src/org/avengers/template/java/javaDast.groovy
+++ src/org/avengers/template/java/javaDast.groovy
@@ -10,5 +10,5 @@ def call(String url, String creds, String branch, String zapVersion){
 
   gitCheckoutPrivate.call(url, creds, branch)
   owaspZapInstallation.call(zapVersion)
-  runZap.call(zapVersion)
+  runZap.call(zapVersion, currentWorkspace)
 }
