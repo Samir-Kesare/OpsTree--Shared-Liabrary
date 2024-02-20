e460880e22b78956a7123628d7f036ae2b0553d9
vyadavP7 <156056349+vyadavP7@users.noreply.github.com>
2024-02-20 15:16:39 +0530

    Create OwaspZapInstallation.groovy

---
+++ src/org/avengers/java/dast/OwaspZapInstallation.groovy
@@ -0,0 +1,12 @@
+package org.avengers.java.dast
+
+def call(String zapVersion) {
+  stage('Install ZAP') {
+    script {
+         // Download and install OWASP ZAP
+          sh "wget https://github.com/zaproxy/zaproxy/releases/download/v${zapVersion}/ZAP_${zapVersion}_Linux.tar.gz"
+          sh "tar -xvf ZAP_${zapVersion}_Linux.tar.gz"
+                    
+                }
+            }
+        }
