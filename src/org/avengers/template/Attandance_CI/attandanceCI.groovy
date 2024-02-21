6164f582fa3a666a5715a77f5c57c0cb47a0e590
vikram445 <79625874+vikram445@users.noreply.github.com>
2024-02-21 21:38:09 +0530

    Update attandanceCI.groovy

--- src/org/avengers/template/Attandance_CI/attandanceCI.groovy
+++ src/org/avengers/template/Attandance_CI/attandanceCI.groovy
@@ -1,1 +1,41 @@
+package org.avengers.template
 
+import org.avengers.python.BugsAnalysisBandit.*
+import org.avengers.python.dependencyScanning.*
+import org.avengers.python.staticCodeAnalysis.*
+import org.avengers.common.*  
+
+def call(String url, String creds, String branch, String depVersion, String javaVersion) {
+    GitCheckoutPrivate gitCheckoutPrivate = new GitCheckoutPrivate()
+    VirtualEnv virtual = new VirtualEnv()
+    Dependencies dep = new Dependencies()
+    StaticCodeAnalysis code = new StaticCodeAnalysis()
+    ArchiveArtifacts arch = new ArchiveArtifacts() 
+    CleanWorkspace cleanWorkspace = new CleanWorkspace()
+    BugsAnalysisBandit bugsAnalysisBandit = new BugsAnalysisBandit()
+    InstallDependencies installDependencies = new InstallDependencies()
+    JavaDownload javaDownload = new JavaDownload()
+    DownloadDependencyCheck downloadDependencyCheck = new DownloadDependencyCheck()
+    DependencyCheck dependencyCheck = new DependencyCheck()
+    Clean clean = new Clean()
+
+    javaDownload.call(javaVersion)
+    downloadDependencyCheck.call(depVersion) 
+    gitCheckoutPrivate.call(url, creds, branch)
+    dependencyCheck.call()
+    clean.call()
+    cleanWorkspace.call()
+    bugsAnalysisBandit.call()
+    installDependencies.call()
+    
+    try {
+        virtual.call()
+        dep.call()
+        code.call()
+    } catch (Exception e) {
+        // Handle any exceptions during static code analysis
+        echo "An error occurred during static code analysis: ${e.message}"
+    } finally {
+        arch.call()
+    }
+}  
