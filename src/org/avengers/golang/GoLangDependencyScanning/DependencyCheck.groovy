55a975b4bd96c818499e35a6540c8acc6d126822
tripathishikha1 <156056746+tripathishikha1@users.noreply.github.com>
2024-02-21 15:02:32 +0530

    Create DependencyCheck.groovy

---
+++ src/org/avengers/golang/GoLangDependencyScanning/DependencyCheck.groovy
@@ -0,0 +1,9 @@
+package org.avengers.golang.GoLangDependencyScanning
+
+def call() {
+    stage('Run Dependency Check') {
+        script {
+           sh "dependency-check/bin/dependency-check.sh --scan /var/lib/jenkins/workspace/ --out dep-check.html"
+        }
+    }
+}
