685f5b6fdd9c569c8fc6335fb41ffa1b74f870a3
vyadavP7 <156056349+vyadavP7@users.noreply.github.com>
2024-02-21 23:48:20 +0530

    Update attandanceCI.groovy

--- src/org/avengers/template/Attandance_CI/attandanceCI.groovy
+++ src/org/avengers/template/Attandance_CI/attandanceCI.groovy
@@ -6,41 +6,55 @@ import org.avengers.python.staticCodeAnalysis.*
 import org.avengers.common.*  
 
 def call(String url, String creds, String branch, String depVersion) {
-    //common
+    //common definition
     def gitCheckoutPrivate = new GitCheckoutPrivate()
     def cleanW = new cleanWorkspace()
 
-    // Static code analysis
+    // Static code analysis definition
     def VirtualEnv = new virtualEnv()
     def Dependencies = new dependencies()
     def StaticCodeAnalysis = new staticCodeAnalysis()
     def archive = new ArchiveArtifacts() 
 
-    // bugs analysis
+    // bugs analysis definition
     def bugsAnalysisBandit = new BugsAnalysisBandit()
     def installDependencies = new InstallDependencies()
 
-    // dependency scanning
+    // dependency scanning definition
     def downloadDependencyCheck = new DownloadDependencyCheck()
     def dependencyCheck = new DependencyCheck()
     def cleandp = new Clean()
 
-    downloadDependencyCheck.call(depVersion) 
+    // static code analysis 
+      try {
     gitCheckoutPrivate.call(url, creds, branch)
-    dependencyCheck.call()
-    cleandp.call()
-    cleanW.call()
-    bugsAnalysisBandit.call()
-    installDependencies.call()
-    
-    try {
-        virtual.call()
-        dep.call()
-        code.call()
+    VirtualEnv.call()
+    Dependencies.call()
+    StaticCodeAnalysis.call()
     } catch (Exception e) {
         // Handle any exceptions during static code analysis
         echo "An error occurred during static code analysis: ${e.message}"
     } finally {
-        arch.call()
+        archive.call()
     }
+
+    
+    // downloadDependencyCheck.call(depVersion) 
+    // gitCheckoutPrivate.call(url, creds, branch)
+    // dependencyCheck.call()
+    // cleandp.call()
+    // cleanW.call()
+    // bugsAnalysisBandit.call()
+    // installDependencies.call()
+    
+    // try {
+    //     virtual.call()
+    //     dep.call()
+    //     code.call()
+    // } catch (Exception e) {
+    //     // Handle any exceptions during static code analysis
+    //     echo "An error occurred during static code analysis: ${e.message}"
+    // } finally {
+    //     arch.call()
+    // }
 }  
