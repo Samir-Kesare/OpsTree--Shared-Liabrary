1b466d2d9c92b4bb0121e892ed2d276fc42f8b57
vyadavP7 <156056349+vyadavP7@users.noreply.github.com>
2024-02-23 15:01:33 +0530

    Update virtENV.groovy

--- src/org/avengers/python/unitTesting/virtENV.groovy
+++ src/org/avengers/python/unitTesting/virtENV.groovy
@@ -1,1 +1,15 @@
+package org.avengers.python.unitTesting
 
+def call(){           
+        stage('Creat Virtual ENV') 
+        {
+            steps 
+            {
+                script 
+                {
+                  sh 'python3 -m venv myenv'
+                  sh '. myenv/bin/activate'
+                }
+            }
+        }
+}
