dd7ecf8617517f1bd261a6b15fd0fc5ae6add09d
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-02-23 14:55:40 +0530

    Update frontendChecks.groovy

--- src/org/avengers/react/frontendChecks.groovy
+++ src/org/avengers/react/frontendChecks.groovy
@@ -2,22 +2,22 @@ package org.avengers.react
 
 def bugAnalysis() {
     stage('Bug Analysis') {
-                steps {
+                // steps {
                     script {
                         sh 'npm install'
                         sh 'npm ci'
                         sh 'echo "module.exports = { extends: \'eslint:recommended\' };" > .eslintrc.js'
                         sh 'npx eslint . --ext .js,.jsx --format html --output-file eslint-report.html || true'
                     }
-                }
+                // }
     }
 }
 
 def dpCheck() {
         stage('Dependency Check') {
-            steps {
+            // steps {
                 sh 'npm install'
                 dependencyCheck additionalArguments: '--project dp-check --scan /var/lib/jenkins/workspace/dp-check  --format HTML', odcInstallation: 'DP-check'
-            }
+            // }
         }
 }
