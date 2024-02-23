53c8ff98dd2046c51da36e9900e3805e1e4567f1
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-23 16:16:23 +0530

    Update EmployeeCi.groovy

--- src/org/avengers/template/employeeCI/EmployeeCi.groovy
+++ src/org/avengers/template/employeeCI/EmployeeCi.groovy
@@ -1,3 +1,22 @@
 package org.avengers.template.employeeCI
 
-import 
+import org.avengers.licenseScanning.licenceScan
+import org.avengers.credScanning.*
+import org.avengers.common.*
+import org.avengers.golang.CodeCompilation.*
+import org.avengers.golang.GoLangDependencyScanning.*
+import org.avengers.golang.bugAnalysis.*
+import org.avengers.golang.unitTesting.*
+
+
+
+
+
+
+def call(Map config = [:],
+
+    def licenceScan = new licenceScan()
+    def gitCheckout = new gitCheckout()
+    def gitLeaks = new GitLeaks()
+    def scan = new Scan()
+    def 
