151ced064f79334e17b86088cba0c0cad4ea4935
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-02-24 00:13:30 +0530

    Update Scan.groovy

--- src/org/avengers/credScanning/Scan.groovy
+++ src/org/avengers/credScanning/Scan.groovy
@@ -1,6 +1,6 @@
 package org.avengers.credScanning
 
-def call(String reportName){
+def call(String reportName) {
   stage('Gitleaks Scan') {
         script {
            sh "./gitleaks detect -r ${reportName}"
