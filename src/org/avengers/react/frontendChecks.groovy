67267f3390f488636f1b4f5faebc66af77284408
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-02-23 18:14:57 +0530

    Update frontendChecks.groovy

--- src/org/avengers/react/frontendChecks.groovy
+++ src/org/avengers/react/frontendChecks.groovy
@@ -27,37 +27,7 @@ stage('code analysis') {
             //steps {
     def scannerHome = tool 'SonarScanner';
     withSonarQubeEnv('sq1') {
-        //sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=frontend-18 -Dsonar.sources=. -Dsonar.host.url=http://10.188.0.3:9000  -Dsonar.login=sqp_d82e793e10438301776a7740f7c4c16a595ece60"
         sh "/var/lib/jenkins/tools/hudson.plugins.sonar.MsBuildSQRunnerInstallation/SonarScanner/sonar-scanner-5.0.1.3006/bin/sonar-scanner -Dsonar.projectKey=frontend-18 -Dsonar.sources=. -Dsonar.host.url=http://10.188.0.3:9000  -Dsonar.login=sqp_d82e793e10438301776a7740f7c4c16a595ece60"
-    }
-            
-        // withSonarQubeEnv(installationName: 'sq1') {
-                
-            // sh 'ls ${scannerHome}'
-            // sh 'echo ${scannerHome}'
-            // withCredentials([string(credentialsId: 'frontend-sonar', variable: 'token')]) {
-             // sonar-scanner \
-
-                // sh '''
-             
-                // /opt/sonarqube/current/bin/linux-x86-64/sonar.sh \
-                //   -Dsonar.projectKey=frontend-18 \
-                //   -Dsonar.sources=. \
-                //   -Dsonar.host.url=http://34.130.229.252:9000 \
-                //   -Dsonar.login=sqp_d82e793e10438301776a7740f7c4c16a595ece60
-                // '''
-                // // }
-            // }
-        }         
+        }
+}         
 }
-// node {
-//   stage('SCM') {
-//     checkout scm
-//   }
-//   stage('SonarQube Analysis') {
-//     def scannerHome = tool 'SonarScanner';
-//     withSonarQubeEnv() {
-//       sh "${scannerHome}/bin/sonar-scanner"
-//     }
-//   }
-// }
