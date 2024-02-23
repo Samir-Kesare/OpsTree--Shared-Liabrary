6abc78f3ad5fdfcf111548d753ff63d1bb951c48
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-02-23 14:23:48 +0530

    Update frontendCI.groovy

--- src/org/avengers/template/frontendCI/frontendCI.groovy
+++ src/org/avengers/template/frontendCI/frontendCI.groovy
@@ -1,1 +1,65 @@
+package org.avengers.template
 
+import org.avengers.licenseScanning.licenceScan
+import org.avengers.credScanning.*
+import org.avengers.common.*
+import org.avengers.java.compile.*
+import org.avengers.java.staticCodeAnalysis.*
+import org.avengers.java.dependencyCheck.*
+import org.avengers.java.unitTesting.*
+import org.avengers.java.bugAnalysis.*
+
+
+def call(Map config = [:], String gitLeaksVersion, String reportName){
+    
+    def licenceScan = new licenceScan()
+    def gitCheckout = new gitCheckout()
+    def javaCompile = new compile()
+    def staticCodeAnalysis = new staticCodeAnalysis()
+    def cleanWorkspace = new cleanWorkspace()
+    def dpCheck = new dpCheck()
+    def javaUnitTesting = new test()
+    def javaBugAnalysis = new bug()
+    def javaPublishHtml = new publishHtml()
+    def gitLeaks = new GitLeaks()
+    def  scan = new Scan()
+    cleanAfterArchive = new CleanAfterArchive()
+    
+    try{
+    gitCheckout.call(branch: config.branch, url: config.url  )
+    gitLeaks.call(gitLeaksVersion)
+    scan.call(reportName)
+    withCredentials([string(credentialsId: 'fossaToken', variable: 'FOSSA_API_KEY')]){
+        licenceScan.installFossa()
+        licenceScan.scan()
+    }
+    javaCompile.call()
+    parallel dpCheck: {
+        dpCheck.call()
+    },
+    staticCodeAnalysis: {
+        staticCodeAnalysis.call()        
+    },
+    bugAnalysis: {
+        javaBugAnalysis.call()
+        javaPublishHtml.call()
+    },
+    unitTesting:{
+        javaUnitTesting.call()   
+    }
+    }
+    catch (e){
+        echo 'Salary CI Failed'
+        cleanWorkspace.call()
+        throw e
+    }
+    finally {
+         def currentResult = currentBuild.result ?: 'SUCCESS'
+        if ((currentResult == 'UNSTABLE')||(currentResult == 'ABORTED')) {
+        cleanWorkspace.call()
+            // echo 'This will run only if the run was marked as unstable'
+        }
+        // cleanAfterArchive.call(reportName) 
+
+    }
+}
