77027cb2763a952136c485d4c0edfc0665622686
vyadavP7 <156056349+vyadavP7@users.noreply.github.com>
2024-02-23 15:42:03 +0530

    Update installDependency.groovy

--- src/org/avengers/python/unitTesting/installDependency.groovy
+++ src/org/avengers/python/unitTesting/installDependency.groovy
@@ -1,15 +1,14 @@
+// installDependency.groovy
 package org.avengers.python.unitTesting
 
-def call(){   
-        stage('Install Dependencies') 
-        {
-            steps 
-            {
-                script 
-                {
-                  sh 'pip install -r requirements.txt'
-                  sh 'pip install pytest pytest-html'
-                }
+def call() {
+    stage('Install Dependencies') {
+        steps {
+            script {
+                sh 'pip install -r requirements.txt'
+                sh 'pip install pytest pytest-html'
             }
         }
+    }
 }
+
