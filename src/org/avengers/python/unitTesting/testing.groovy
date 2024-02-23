b7d67c1d1863c078371fdadb6a6e48bc0239fd75
vyadavP7 <156056349+vyadavP7@users.noreply.github.com>
2024-02-23 15:45:33 +0530

    Update testing.groovy

--- src/org/avengers/python/unitTesting/testing.groovy
+++ src/org/avengers/python/unitTesting/testing.groovy
@@ -1,19 +1,10 @@
 package org.avengers.python.unitTesting
 
-def call(){   
-        stage('Unit Test') 
-        {
-            steps 
-            {
-                script 
-                {
-                    sh '. myenv/bin/activate'
-                    //sh 'pytest router/tests/test_c
-                    sh 'pytest router/tests/test_cache.py'
-                    //sh 'pytest /var/lib/jenkins/workspace/unit_test '
-                    
-                    
-                }
-            }
+def call() {
+    stage('Unit Test') {
+        script {
+            sh '. myenv/bin/activate'
+            sh 'pytest router/tests/test_cache.py'
         }
+    }
 }
