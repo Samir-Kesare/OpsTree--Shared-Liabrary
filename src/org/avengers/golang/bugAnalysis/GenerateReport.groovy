55d28cd2f13a36aacad139e2e1dce0d8ee810ad5
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-23 18:43:51 +0530

    Update GenerateReport.groovy

--- src/org/avengers/golang/bugAnalysis/GenerateReport.groovy
+++ src/org/avengers/golang/bugAnalysis/GenerateReport.groovy
@@ -5,6 +5,8 @@ def call() {
         script {
              // Run golangci-lint with the --out-format option to specify the output format
              sh 'golangci-lint run ./... --out-format html > BugAnalysisreport.html || true'
+             sh 'go test ./... -coverprofile=coverage.out || true'
+             sh 'go tool cover -html=coverage.out -o coverage.html || true'
         }
     }
 }
