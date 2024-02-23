7bc3a9ca0451431422f1671430207cfde594bf72
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-23 18:27:25 +0530

    Update GenerateReport.groovy

--- src/org/avengers/golang/bugAnalysis/GenerateReport.groovy
+++ src/org/avengers/golang/bugAnalysis/GenerateReport.groovy
@@ -4,7 +4,7 @@ def call() {
     stage('Generate HTML Report') {
         script {
              // Run golangci-lint with the --out-format option to specify the output format
-             sh 'golangci-lint run ./... --out-format html > report.html || true'
+             sh 'golangci-lint run ./... --out-format html > BugAnalysisreport.html || true'
         }
     }
 }
