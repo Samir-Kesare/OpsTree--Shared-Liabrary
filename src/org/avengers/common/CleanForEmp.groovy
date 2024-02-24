35c580a6ef96915d18bd5fac3d610c4f87cafc09
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-24 13:08:30 +0530

    Update CleanForEmp.groovy

--- src/org/avengers/common/CleanForEmp.groovy
+++ src/org/avengers/common/CleanForEmp.groovy
@@ -1,1 +1,15 @@
 
+package org.avengers.common
+
+def call(String gitLeaksReport, String uniTestReport, String BugAnalysisreport, String depreport){
+  stage('Archive and Clean Workspace') {
+        script {
+           archiveArtifacts artifacts: "**/${gitLeaksReport}"
+           archiveArtifacts artifacts: "**/${uniTestReport}"
+           archiveArtifacts artifacts: "**/${BugAnalysisreport}"
+           archiveArtifacts artifacts: "**/${depreport}"
+           cleanWs()
+        }
+    }
+  
+}
