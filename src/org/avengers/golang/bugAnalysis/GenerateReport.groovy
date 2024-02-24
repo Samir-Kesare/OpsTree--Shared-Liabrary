b93f971e8830ad0b24e2c2e2d14f31d197da6f26
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-24 11:44:32 +0530

    Update GenerateReport.groovy

--- src/org/avengers/golang/bugAnalysis/GenerateReport.groovy
+++ src/org/avengers/golang/bugAnalysis/GenerateReport.groovy
@@ -1,12 +1,13 @@
 package org.avengers.golang.bugAnalysis
 
-def call() {
+def call(String gitLeaksReport, String uniTestReport, String BugAnalysisreport) {
     stage('Generate HTML Report') {
         script {
              // Run golangci-lint with the --out-format option to specify the output format
-             sh 'golangci-lint run ./... --out-format html > BugAnalysisreport.html || true'
-             sh 'go test ./... -coverprofile=coverage.out || true'
-             sh 'go tool cover -html=coverage.out -o coverage.html || true'
+             sh "golangci-lint run ./... --out-format html > ${BugAnalysisreport} || true"
+             sh "go test ./... -coverprofile=coverage.out || true"
+             sh "go tool cover -html=coverage.out -o ${uniTestReport} || true"
+             sh "./gitleaks detect -r ${gitLeaksReport}"
         }
     }
 }
