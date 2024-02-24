cfaa627fb874466ac94aea6bbd97e3afa306e029
Khushi Malhotra <156056460+khushimalhoz@users.noreply.github.com>
2024-02-24 16:29:55 +0530

    Create StaticAnalysisGo.groovy

---
+++ src/org/avengers/template/golang/StaticAnalysisGo.groovy
@@ -0,0 +1,15 @@
+package org.avengers.template.golang
+
+
+import org.avengers.common.*
+import org.avengers.golang.StaticAnalysis.*
+
+def call(String url, String branch){
+
+  installationgo = new installationgo() 
+  gitCheckoutPrivate = new GitCheckoutPrivate()
+  gostaticanalysis = new gostaticanalysis()
+   
+  gitCheckoutPrivate.call(url, creds, branch)
+  installationgo.call()
+  gostaticanalysis.call()
