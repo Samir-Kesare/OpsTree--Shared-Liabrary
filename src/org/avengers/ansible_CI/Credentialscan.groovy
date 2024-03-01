cb62719b5914fbc2931a49670a36c2cd30a89e49
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-03-01 10:42:42 +0530

    Update Credentialscan.groovy

--- src/org/avengers/ansible_CI/Credentialscan.groovy
+++ src/org/avengers/ansible_CI/Credentialscan.groovy
@@ -1,10 +1,10 @@
 package org.avengers.ansible_CI
-def call() {
+def call(String gitleaksVersion) {
   stage('Credential Scanning') {
             script {
-                sh 'wget https://github.com/gitleaks/gitleaks/releases/download/v8.18.2/gitleaks_8.18.2_linux_x64.tar.gz'
+                sh 'wget https://github.com/gitleaks/gitleaks/releases/download/v${gitleaksVersion}/gitleaks_${gitleaksVersion}_linux_x64.tar.gz'
                 // Extract Gitleaks
-                sh 'tar xvzf gitleaks_8.18.2_linux_x64.tar.gz'
+                sh 'tar xvzf gitleaks_${gitleaksVersion}_linux_x64.tar.gz'
                 sh './gitleaks detect -r credScanReport.json'
             }
         }
