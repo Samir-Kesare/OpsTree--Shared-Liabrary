cd0094a68591c403a457a7300308901e19009426
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-03-01 11:02:17 +0530

    Create gitTag.groovy

---
+++ src/org/avengers/ansible_CI/gitTag.groovy
@@ -0,0 +1,9 @@
+package org.avengers.golang.dast
+def call(String tagVersion ) {
+  stage('Git Tag Stage') {
+            script {
+                // Tag the version
+                sh 'git tag -a v${tagVersion} -m "Version ${tagVersion}"'
+            }
+        }
+}
