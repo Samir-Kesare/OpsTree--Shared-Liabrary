51f5958139e0413aa58a7d846707a8f2ac8353f5
Khushi Malhotra <156056460+khushimalhoz@users.noreply.github.com>
2024-03-03 22:05:43 +0530

    Update terraformCI.groovy

--- src/org/avengers/template/terraform/terraformCI.groovy
+++ src/org/avengers/template/terraform/terraformCI.groovy
@@ -6,10 +6,10 @@ import org.avengers.common.*
 
 def call(String url, String cred, String branch, String rootPath, String childPath, String version) {
 
-    def git = new org.mygurukulam.common.Checkout()
-    def tf = new org.mygurukulam.terraform.Action()
-    def lint = new org.mygurukulam.terraform.Linting()
-    def cleanup = new org.mygurukulam.common.Cleanup()
+    gitCheckoutPrivate = new GitCheckoutPrivate()
+    tf = new org.mygurukulam.terraform.Action()
+    lint = new org.avengers.terraform_CICD.linting()
+    cleanup = new org.mygurukulam.common.Cleanup()
 
     cleanup.call()
     git.call(url, cred, branch)
