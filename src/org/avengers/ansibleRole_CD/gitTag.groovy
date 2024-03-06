7d10cfbaa6a6e1f2db95c3d6e05e7f9978b06588
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-03-06 17:58:18 +0530

    Create gitTag.groovy

---
+++ src/org/avengers/ansibleRole_CD/gitTag.groovy
@@ -0,0 +1,9 @@
+package org.avengers.ansibleRole_CD
+def call(String tagVersion) {
+  stage('Git Tag Stage') {
+            script {
+                // Tag the version
+                sh 'git tag -a v${tagVersion} -m "Version ${tagVersion}"'
+            }
+        }
+}
