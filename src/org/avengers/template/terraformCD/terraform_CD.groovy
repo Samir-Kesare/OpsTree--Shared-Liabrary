685d5e54f6c4fcca32832b4fc19662a846f5fd6c
Khushi Malhotra <156056460+khushimalhoz@users.noreply.github.com>
2024-03-06 19:29:43 +0530

    Update terraform_CD.groovy

--- src/org/avengers/template/terraformCD/terraform_CD.groovy
+++ src/org/avengers/template/terraformCD/terraform_CD.groovy
@@ -1,1 +1,22 @@
+package org.avengers.template.terraformCD
 
+import org.avengers.common.*
+import org.avengers.terraform_CICD.*
+
+def call(String url, String creds, String branch, String rootPath, String childPath, String reportName1, String reportName2){
+
+    variablization = new action()
+    gitCheckoutPrivate = new GitCheckoutPrivate()
+    cleanWorkspace = new cleanWorkspace()
+ //   initialization = new init()
+    deploying = new deploy()
+    
+  
+
+    cleanWorkspace.call()
+    gitCheckoutPrivate.call(url, creds, branch)
+    variablization.call(rootPath, childPath)
+  // initialization.call(rootPath, childPath)
+    deploying.call(rootPath, childPath) 
+
+}
