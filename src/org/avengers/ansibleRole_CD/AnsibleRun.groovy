bc20a34b436e525c60b3340bb05e220c196d333b
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-03-06 17:56:49 +0530

    Update and rename AnsibleRun.groovy to Syntaxcheck.groovy

--- src/org/avengers/ansibleRole_CD/AnsibleRun.groovy
+++
@@ -1,9 +0,0 @@
-package org.avengers.ansibleRole_CD
-def call() {
-  stage('Ansible Run Stage') {
-            script {
-                // Run the ansible playbook
-                sh 'ansible-playbook /var/lib/jenkins/workspace/Ansible_CD/jenkins_playbook/install_debian.yml'
-            }
-        }
-    }
---
+++ src/org/avengers/ansibleRole_CD/Syntaxcheck.groovy
@@ -0,0 +1,15 @@
+package org.avengers.ansibleRole_CD
+def call() {
+  stage('Syntax Checking Stage') {
+           
+                script {
+                    try {
+                        // Run ansible-playbook with the --syntax-check option to validate playbook syntax
+                        sh 'ansible-playbook /var/lib/jenkins/workspace/Ansible_CD/jenkins_playbook/install_debian.yml --syntax-check > syntaxcheck_report.txt'
+                    } catch (Exception e) {
+                        // Ignore syntax check errors
+                        echo 'Syntax checking completed with errors. Ignoring...'
+                    }
+                }
+            }
+        }
