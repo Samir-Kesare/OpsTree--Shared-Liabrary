c14ab147c6b3167740014d856de1dad2717d5c84
Khushi Malhotra <156056460+khushimalhoz@users.noreply.github.com>
2024-03-06 15:05:55 +0530

    Update checkov.groovy

--- src/org/avengers/terraform_CICD/checkov.groovy
+++ src/org/avengers/terraform_CICD/checkov.groovy
@@ -3,6 +3,7 @@ package org.avengers.terraform_CICD
 def call(String rootPath, String childPath) {
     stage('checkov') {
         script {
+            sh "pip install checkov"
             sh "cd ${rootPath}/${childPath} && checkov --directory . || true"
         }
     }
