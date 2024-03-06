28dfc4bbf71231693f84cf00f41c39a7b94ae347
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-03-06 19:40:30 +0530

    Update gitPushTag.groovy

--- src/org/avengers/ansibleRole_CD/gitPushTag.groovy
+++ src/org/avengers/ansibleRole_CD/gitPushTag.groovy
@@ -1,10 +1,10 @@
 package org.avengers.ansibleRole_CD
-def call() {
+def call(String Path) {
   stage('Git Push Version Stage') {
             script {
                 // Push the tagged version to the remote repository
                 withCredentials([usernamePassword(credentialsId: 'f941d7e1-ff1a-4bca-835c-a458a1b3d96a', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-                    sh 'git push https://username:${PASSWORD}@github.com/CodeOps-Hub/Jenkins-Playbook.git v1.1'
+                    sh 'git push ${Path}'
                 }
             }
         }
