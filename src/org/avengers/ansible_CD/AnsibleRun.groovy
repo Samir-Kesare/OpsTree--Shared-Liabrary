5de0ab0a6b3a3c8a650825f0ff0a15c90290bd28
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-03-06 20:17:20 +0530

    Update AnsibleRun.groovy

--- src/org/avengers/ansible_CD/AnsibleRun.groovy
+++ src/org/avengers/ansible_CD/AnsibleRun.groovy
@@ -1,9 +1,9 @@
 package org.avengers.ansible_CD
-def call() {
+def call(String path) {
   stage('Ansible Run Stage') {
             script {
                 // Run the ansible playbook
-                sh 'ansible-playbook /var/lib/jenkins/workspace/Ansible_CD/jenkins_playbook/install_debian.yml'
+                sh 'ansible-playbook ${path}'
             }
         }
     }
