75f8d9a41c792eb45ab0b5dff100540da636cd56
Khushi Malhotra <156056460+khushimalhoz@users.noreply.github.com>
2024-03-06 12:19:53 +0530

    Update action.groovy

--- src/org/avengers/terraform_CICD/action.groovy
+++ src/org/avengers/terraform_CICD/action.groovy
@@ -1,7 +1,7 @@
 package org.avengers.terraform_CICD
 
 def call(String rootPath, String childPath, String action) {
-    stage("Terraform ${action}") {
+    stage("Terraform action") {
         script {
             sh "cd ${rootPath}/${childPath}"
         }
