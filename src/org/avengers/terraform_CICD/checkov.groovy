47162e68173b15a6b94bcbb2e7616a8dc1b734cc
Khushi Malhotra <156056460+khushimalhoz@users.noreply.github.com>
2024-03-06 17:11:49 +0530

    Update checkov.groovy

--- src/org/avengers/terraform_CICD/checkov.groovy
+++ src/org/avengers/terraform_CICD/checkov.groovy
@@ -5,7 +5,7 @@ def call(String rootPath, String childPath) {
         script {
             //sh "pip install checkov"
             sh "python3 -m pip install checkov"
-            sh "echo "export PATH=\"`python3 -m site --user-base`/bin:\$PATH\"" >> ~/.bashrc"
+            sh 'echo "export PATH=\"`python3 -m site --user-base`/bin:\$PATH\"" >> ~/.bashrc'
             sh "source ~/.bashrc"
             sh "cd ${rootPath}/${childPath} && checkov --directory . || true"
         }
