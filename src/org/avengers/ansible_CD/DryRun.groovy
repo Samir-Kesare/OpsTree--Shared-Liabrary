d124d02bc20c83d2e0ab427e6d9b21724aa0a7a7
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-03-07 11:49:24 +0530

    Update DryRun.groovy

--- src/org/avengers/ansible_CD/DryRun.groovy
+++ src/org/avengers/ansible_CD/DryRun.groovy
@@ -1,9 +1,9 @@
 package org.avengers.ansible_CD
-def call() {
+def call(String path) {
   stage('Dry Run Stage') {
             script {
                 // Perform a dry run 
-                sh 'ansible-playbook /var/lib/jenkins/workspace/Ansible_CD/jenkins_playbook/install_debian.yml --check'
+                sh 'ansible-playbook ${path} --check'
             }
         }
 }
