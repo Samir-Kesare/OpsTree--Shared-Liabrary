cbddee0f57f0d8c78c5d3c348d351e9978881830
Khushi Malhotra <156056460+khushimalhoz@users.noreply.github.com>
2024-03-07 20:41:32 +0530

    Update deploy.groovy

--- src/org/avengers/terraform_CICD/deploy.groovy
+++ src/org/avengers/terraform_CICD/deploy.groovy
@@ -3,13 +3,13 @@ package org.avengers.terraform_CICD
 def call(String rootPath, String childPath) {
     stage("Terraform Plan") {
         script {
-            sh "terraform plan"
+            sh "cd ${rootPath}/${childPath} && terraform plan"
         }
     }
 
     stage("Terraform Apply") {
         script {
-            sh "terraform apply -auto-approve"
+            sh "cd ${rootPath}/${childPath} && terraform apply -auto-approve"
         }
     }
 }
