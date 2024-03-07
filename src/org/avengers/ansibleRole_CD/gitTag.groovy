eaae312e38426559448bf0608bdf00307f0bb9aa
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-03-07 19:21:15 +0530

    Update gitTag.groovy

--- src/org/avengers/ansibleRole_CD/gitTag.groovy
+++ src/org/avengers/ansibleRole_CD/gitTag.groovy
@@ -3,7 +3,7 @@ def call(String tagVersion) {
   stage('Git Tag Stage') {
             script {
                 // Tag the version
-                sh "git tag -a v${tagVersion} -m "Version ${tagVersion}""
+                sh "git tag -a v${tagVersion} -m 'Version ${tagVersion}'"
             }
         }
 }
