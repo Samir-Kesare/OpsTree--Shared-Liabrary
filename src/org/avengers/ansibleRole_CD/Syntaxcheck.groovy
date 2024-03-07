0bc25ee6da0cbe57ceb77967d2499983ca9c601e
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-03-07 11:43:34 +0530

    Update Syntaxcheck.groovy

--- src/org/avengers/ansibleRole_CD/Syntaxcheck.groovy
+++ src/org/avengers/ansibleRole_CD/Syntaxcheck.groovy
@@ -5,7 +5,7 @@ def call() {
                 script {
                     try {
                         // Run ansible-playbook with the --syntax-check option to validate playbook syntax
-                        sh 'ansible-playbook /var/lib/jenkins/workspace/Ansible_CD/jenkins_playbook/install_debian.yml --syntax-check > syntaxcheck_report.txt'
+                        sh 'ansible-playbook ${path} --syntax-check > syntaxcheck_report.txt'
                     } catch (Exception e) {
                         // Ignore syntax check errors
                         echo 'Syntax checking completed with errors. Ignoring...'
