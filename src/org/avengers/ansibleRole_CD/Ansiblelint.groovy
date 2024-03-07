ed73c3e3848e636e0e1ffbc10794d2389352f1a0
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-03-07 11:42:06 +0530

    Update Ansiblelint.groovy

--- src/org/avengers/ansibleRole_CD/Ansiblelint.groovy
+++ src/org/avengers/ansibleRole_CD/Ansiblelint.groovy
@@ -1,11 +1,11 @@
 package org.avengers.ansibleRole_CD
-def call() {
+def call(String path) {
     stage('Linting Stage') {
        
             script {
                 try {
                     // Use Ansible Lint to perform linting on your Ansible playbook
-                    sh 'ansible-lint /var/lib/jenkins/workspace/Ansible_CD/jenkins_playbook/install_debian.yml -q > lint_report.txt'
+                    sh 'ansible-lint ${path} -q > lint_report.txt'
                 } catch (Exception e) {
                     // Ignore linting errors
                     echo 'Linting completed with errors. Ignoring...'
