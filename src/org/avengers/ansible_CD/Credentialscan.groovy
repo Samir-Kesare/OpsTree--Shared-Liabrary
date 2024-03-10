fd3ab78d2dd4b67cc3c65b71adb76d0ac656832a
aakashtripathi-snaatak <156056344+aakashtripathi-snaatak@users.noreply.github.com>
2024-03-10 15:23:20 +0530

    Update Credentialscan.groovy

--- src/org/avengers/ansible_CD/Credentialscan.groovy
+++ src/org/avengers/ansible_CD/Credentialscan.groovy
@@ -2,10 +2,16 @@ package org.avengers.ansible_CD
 def call(String gitleaksVersion) {
   stage('Credential Scanning') {
             script {
+              try {
                 sh "wget https://github.com/gitleaks/gitleaks/releases/download/v${gitleaksVersion}/gitleaks_${gitleaksVersion}_linux_x64.tar.gz"
                 // Extract Gitleaks
                 sh "tar xvzf gitleaks_${gitleaksVersion}_linux_x64.tar.gz"
                 sh "./gitleaks detect -r credScanReport.json"
+              }
+              catch {
+              sh ' echo " Please check the cred scan report " '
+              }
+              
             }
         }
 }
