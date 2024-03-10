9042602e0415f9612390223ea7aa83fdfeae695a
Khushi Malhotra <156056460+khushimalhoz@users.noreply.github.com>
2024-03-10 20:05:33 +0530

    Update deploy.groovy

--- src/org/avengers/terraform_CICD/deploy.groovy
+++ src/org/avengers/terraform_CICD/deploy.groovy
@@ -1,15 +1,51 @@
 package org.avengers.terraform_CICD
 
-def call(String rootPath, String childPath) {
+def call(String rootPath, String childPath, String ACTION) {
     stage("Terraform Plan") {
         script {
             sh "cd ${rootPath}/${childPath} && terraform plan"
         }
     }
 
-    stage("Terraform Apply") {
-        script {
-            sh "cd ${rootPath}/${childPath} && terraform apply -auto-approve"
+    stage('Want to Perform Terraform Apply') {
+            when {
+                expression { params.ACTION == 'Proceed' }
+            }
+            steps {
+                // Prompt for approval before applying changes
+                input "Do you want to apply Terraform changes?"
+            }
+        }
+        
+        stage('Review and Approve Destroy') {
+            when {
+                expression { params.ACTION == 'Abort' }
+            }
+            steps {
+                // Prompt for approval before destroying resources
+                input "Do you want to destroy Terraform resources?"
+            }
+        }
+
+    stage('Proceed or Abort') {
+            steps {
+                script {
+                    if (params.ACTION == 'Proceed') {
+                        sh 'terraform apply -auto-approve'
+                    } else if (params.ACTION == 'Abort') {
+                         echo 'Failed!'
+                    }
+                }
+            }
         }
     }
-}
+
+ //   stage("Terraform Apply") {
+ //       script {
+//            sh "cd ${rootPath}/${childPath} && terraform apply -auto-approve"
+ //       }
+//    }
+//}
+
+
+
