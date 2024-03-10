258b73a9c2cc782bbf3d318edad3942a1937e306
Khushi Malhotra <156056460+khushimalhoz@users.noreply.github.com>
2024-03-10 21:50:46 +0530

    Update deploy.groovy

--- src/org/avengers/terraform_CICD/deploy.groovy
+++ src/org/avengers/terraform_CICD/deploy.groovy
@@ -7,72 +7,16 @@ def call(String rootPath, String childPath, String ACTION) {
         }
     }
 
-    // stage('Want to Perform Terraform Apply') {
-    //         when {
-    //             expression { params.ACTION == 'Proceed' }
-    //         }
-    //         steps {
-    //             // Prompt for approval before applying changes
-    //             input "Do you want to apply Terraform changes?"
-    //         }
-    //     }
-        
-    //     stage('Review and Approve Destroy') {
-    //         when {
-    //             expression { params.ACTION == 'Abort' }
-    //         }
-    //         steps {
-    //             // Prompt for approval before destroying resources
-    //             input "Do you want to destroy Terraform resources?"
-    //         }
-    //     }
-
-    //  stage('Want to Perform Terraform Apply') {
-    //     script {
-    //         if (ACTION == 'Proceed') {
-    //             input "Do you want to apply Terraform changes?"
-    //         }
-    //     }
-    // }
-    
-    // stage('Review and Approve Destroy') {
-    //     script {
-    //         if (ACTION == 'Abort') {
-    //             input "Do you want to destroy Terraform resources?"
-    //         }
-    //     }
-    // }
-
-    // stage('Proceed or Abort') {
-    //        // steps {
-    //             script {
-    //                 if (params.ACTION == 'Proceed') {
-    //                     sh 'terraform apply "tfplan.out" -auto-approve'
-    //                 } else if (params.ACTION == 'Abort') {
-    //                      echo 'Failed!'
-    //                 }
-    //             }
-    //       //  }
-    //     }
-    // }
-
- //   stage("Terraform Apply") {
- //       script {
-//            sh "cd ${rootPath}/${childPath} && terraform apply -auto-approve"
- //       }
-//    }
-//}
-
-        stage('Approval For Apply') {
+    stage('Approval For Apply') {
             script {
                 // Prompt for approval before applying changes
                 input "Do you want to apply Terraform changes?"
             }
         }
-        stage('Terraform Apply') {
+    stage('Terraform Apply') {
             script {
                 // Run Terraform apply
-                sh 'terraform apply -auto-approve'
+                sh 'cd ${rootPath}/${childPath} && terraform apply -auto-approve'
             }
         }
        
