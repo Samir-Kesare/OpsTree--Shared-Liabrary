b0e0d0d34d7e2f1a33c95d3773f279f6ff6a80be
Khushi Malhotra <156056460+khushimalhoz@users.noreply.github.com>
2024-03-10 21:34:46 +0530

    Update deploy.groovy

--- src/org/avengers/terraform_CICD/deploy.groovy
+++ src/org/avengers/terraform_CICD/deploy.groovy
@@ -3,7 +3,7 @@ package org.avengers.terraform_CICD
 def call(String rootPath, String childPath, String ACTION) {
     stage("Terraform Plan") {
         script {
-            sh "cd ${rootPath}/${childPath} && terraform plan -out=tfplan.out"
+            sh "cd ${rootPath}/${childPath} && terraform plan"
         }
     }
 
@@ -27,34 +27,34 @@ def call(String rootPath, String childPath, String ACTION) {
     //         }
     //     }
 
-     stage('Want to Perform Terraform Apply') {
-        script {
-            if (ACTION == 'Proceed') {
-                input "Do you want to apply Terraform changes?"
-            }
-        }
-    }
+    //  stage('Want to Perform Terraform Apply') {
+    //     script {
+    //         if (ACTION == 'Proceed') {
+    //             input "Do you want to apply Terraform changes?"
+    //         }
+    //     }
+    // }
     
-    stage('Review and Approve Destroy') {
-        script {
-            if (ACTION == 'Abort') {
-                input "Do you want to destroy Terraform resources?"
-            }
-        }
-    }
+    // stage('Review and Approve Destroy') {
+    //     script {
+    //         if (ACTION == 'Abort') {
+    //             input "Do you want to destroy Terraform resources?"
+    //         }
+    //     }
+    // }
 
-    stage('Proceed or Abort') {
-           // steps {
-                script {
-                    if (params.ACTION == 'Proceed') {
-                        sh 'terraform apply "tfplan.out" -auto-approve'
-                    } else if (params.ACTION == 'Abort') {
-                         echo 'Failed!'
-                    }
-                }
-          //  }
-        }
-    }
+    // stage('Proceed or Abort') {
+    //        // steps {
+    //             script {
+    //                 if (params.ACTION == 'Proceed') {
+    //                     sh 'terraform apply "tfplan.out" -auto-approve'
+    //                 } else if (params.ACTION == 'Abort') {
+    //                      echo 'Failed!'
+    //                 }
+    //             }
+    //       //  }
+    //     }
+    // }
 
  //   stage("Terraform Apply") {
  //       script {
@@ -63,5 +63,40 @@ def call(String rootPath, String childPath, String ACTION) {
 //    }
 //}
 
-
-
+        stage('Approval For Apply') {
+            when {
+                expression { params.ACTION == 'apply' }
+            }
+            steps {
+                // Prompt for approval before applying changes
+                input "Do you want to apply Terraform changes?"
+            }
+        }
+        stage('Terraform Apply') {
+            when {
+                expression { params.ACTION == 'apply' }
+            }
+            steps {
+                // Run Terraform apply
+                sh 'terraform apply -auto-approve'
+            }
+        }
+        stage('Approval for Destroy') {
+            when {
+                expression { params.ACTION == 'destroy' }
+            }
+            steps {
+                // Prompt for approval before destroying resources
+                input "Do you want to Terraform Destroy?"
+            }
+        }
+        stage('Terraform Destroy') {
+            when {
+                expression { params.ACTION == 'destroy' }
+            }
+            steps {
+                // Destroy Infra
+                sh 'terraform destroy -auto-approve'
+            }
+        }
+   }
