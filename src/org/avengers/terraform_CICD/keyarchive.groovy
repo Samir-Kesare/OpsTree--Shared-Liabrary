407a6947254c1578b16ba50d8ff7eab433ab4b26
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-03-19 00:37:32 +0530

    Update keyarchive.groovy

--- src/org/avengers/terraform_CICD/keyarchive.groovy
+++ src/org/avengers/terraform_CICD/keyarchive.groovy
@@ -1,1 +1,11 @@
 
+package org.avengers.terraform_CICD
+
+def call() {
+    stage("Pem Key Archive") {
+        script {
+            sh "cd ${rootPath}/${childPath}"
+            archiveArtifacts artifacts: '*.pem', allowEmptyArchive: true
+        }
+    }
+}
