8de5083efbea09438a8c7b879c179bd7992e7e8d
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-03-19 01:05:30 +0530

    Update deploy.groovy

--- src/org/avengers/terraform_CICD/deploy.groovy
+++ src/org/avengers/terraform_CICD/deploy.groovy
@@ -1,3 +1,27 @@
+// package org.avengers.terraform_CICD
+
+// def call(String rootPath, String childPath, String ACTION) {
+//     stage("Terraform Plan") {
+//         script {
+//             sh "cd ${rootPath}/${childPath} && terraform plan"
+//         }
+//     }
+
+//     stage('Approval For Apply') {
+//             script {
+//                 // Prompt for approval before applying changes
+//                 input "Do you want to apply Terraform changes?"
+//             }
+//         }
+//     stage('Terraform Apply') {
+//             script {
+//                 // Run Terraform apply
+//                 sh 'cd ${rootPath}/${childPath} && terraform apply -auto-approve'
+//             }
+//         }
+       
+//    }
+
 package org.avengers.terraform_CICD
 
 def call(String rootPath, String childPath, String ACTION) {
@@ -7,17 +31,20 @@ def call(String rootPath, String childPath, String ACTION) {
         }
     }
 
-    stage('Approval For Apply') {
+    if (ACTION == 'apply') {
+        stage('Approval For Apply') {
             script {
                 // Prompt for approval before applying changes
                 input "Do you want to apply Terraform changes?"
             }
         }
-    stage('Terraform Apply') {
+        stage('Terraform Apply') {
             script {
                 // Run Terraform apply
                 sh 'cd ${rootPath}/${childPath} && terraform apply -auto-approve'
             }
         }
-       
-   }
+    } else {
+        echo "Skipping Terraform apply since action is not set to 'apply'"
+    }
+}
