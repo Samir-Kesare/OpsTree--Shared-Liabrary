64abfefc099d54cd9cc804dc29b96c0283000645
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-03-21 00:55:43 +0530

    Update cicd.groovy

--- src/org/avengers/template/Module_CI_CD/cicd.groovy
+++ src/org/avengers/template/Module_CI_CD/cicd.groovy
@@ -1,28 +1,10 @@
-// package org.avengers.template.Module_CI_CD
-
-
-// import org.avengers.common.*
-// import org.avengers.Module_CI_CD.*
-
-// def call(String url, String creds, String branch, String rootPath, String childPath, String tagVersion, String gitpath){
-
-    
-//     gitCheckoutPrivate = new GitCheckoutPrivate()
-//     ci = new ci()
-//     cd = new cd()
-    
-//     gitCheckoutPrivate.call(url, creds, branch)
-//     ci.call(rootPath, childPath, tagVersion)
-//     cd.call(gitpath, creds, tagVersion)
-// }
-
 package org.avengers.template.Module_CI_CD
 
 
 import org.avengers.common.*
 import org.avengers.Module_CI_CD.*
 
-def call(String url, String creds, String branch, String rootPath, String childPath, String tagVersion, String gitpath, boolean runTerraformAction, boolean runTerraformInit, boolean runTerraformFmt, boolean runTerraformValidate, boolean runStaticCodeAnalysis, boolean runCheckov, boolean runTerraformPlan, boolean runGitTag){
+def call(String url, String creds, String branch, String rootPath, String childPath, String tagVersion, String gitpath){
 
     
     gitCheckoutPrivate = new GitCheckoutPrivate()
@@ -30,6 +12,7 @@ def call(String url, String creds, String branch, String rootPath, String childP
     cd = new cd()
     
     gitCheckoutPrivate.call(url, creds, branch)
-    ci.call(rootPath, childPath, tagVersion, runTerraformAction, runTerraformInit, runTerraformFmt, runTerraformValidate, runStaticCodeAnalysis, runCheckov, runTerraformPlan, runGitTag)
+    ci.call(rootPath, childPath, tagVersion)
     cd.call(gitpath, creds, tagVersion)
 }
+
