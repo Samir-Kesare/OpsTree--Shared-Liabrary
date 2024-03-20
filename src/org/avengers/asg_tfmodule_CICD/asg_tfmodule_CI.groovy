1979e09dcc9be6be1f950bfe999432f5516e8f25
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-03-20 10:30:03 +0530

    Update asg_tfmodule_CI.groovy

--- src/org/avengers/asg_tfmodule_CICD/asg_tfmodule_CI.groovy
+++ src/org/avengers/asg_tfmodule_CICD/asg_tfmodule_CI.groovy
@@ -1,41 +1,41 @@
 package org.avengers.asg_tfmodule_CICD
 
-def call(String rootPath, String childPath) {
+def call(String rootPath, String childPath, String tagVersion) {
     stage("Terraform action") {
         script {
             sh "cd ${rootPath}/${childPath}"
         }
     }
-}
 
-def call(String rootPath, String childPath) {
+
+
     stage('checkov') {
         script {
             // Stage to run Checkov for Terraform
             sh "cd ${rootPath}/${childPath} && /var/lib/jenkins/.local/bin/checkov -d . -s --output-file-path . --skip-path ./tflint_report.jsonֿ"
         }
     }
-}
 
-def call(String rootPath, String childPath) {
+
+
     stage('Terraform fmt') {
         script {
             // Stage to format Terraform files
             sh "cd ${rootPath}/${childPath} && terraform fmt"
         }
     }
-}
 
-def call(String rootPath, String childPath) {
+
+
     stage("Pem Key Archive") {
         script {
             // Archive PEM key artifacts
             archiveArtifacts artifacts: "${rootPath}/${childPath}/*.pem", allowEmptyArchive: true
         }
     }
-}
 
-def call(String rootPath, String childPath) {
+
+
     stage('Static Code Analysis') {
         script {
             // Install TFLint and run static code analysis
@@ -43,26 +43,24 @@ def call(String rootPath, String childPath) {
             sh "cd ${rootPath}/${childPath} && tflint --format default | tee tflint_report.json"
         }
     }
-}
 
-def call(String rootPath, String childPath) {
+
+
     stage('Terraform Validate') {
         script {
             // Validate Terraform configurations
             sh "cd ${rootPath}/${childPath} && terraform validate"
         }
     }
-}
 
-def call(String rootPath, String childPath) {
+
     stage('Terraform init') {
         script {
             // Initialize Terraform working directory
             sh "cd ${rootPath}/${childPath} && terraform init"
         }
     }
-}
-def call(String tagVersion) {
+
   stage('Git Tag Stage') {
             script {
                 // Tag the version
