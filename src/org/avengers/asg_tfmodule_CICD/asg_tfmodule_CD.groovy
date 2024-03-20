ee09dcea707c97b75e9716732ba34b735ef43f18
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-03-20 08:51:41 +0530

    Update asg_tfmodule_CD.groovy

--- src/org/avengers/asg_tfmodule_CICD/asg_tfmodule_CD.groovy
+++ src/org/avengers/asg_tfmodule_CICD/asg_tfmodule_CD.groovy
@@ -1,1 +1,26 @@
+package org.avengers.terraform_CICD
 
+def call(String rootPath, String childPath, String ACTION) {
+    stage("Terraform Plan") {
+        script {
+            sh "cd ${rootPath}/${childPath} && terraform plan"
+        }
+    }
+
+    if (ACTION == 'apply') {
+        stage('Approval For Apply') {
+            script {
+                // Prompt for approval before applying changes
+                input "Do you want to apply Terraform changes?"
+            }
+        }
+        stage('Terraform Apply') {
+            script {
+                // Run Terraform apply
+                sh 'cd ${rootPath}/${childPath} && terraform apply -auto-approve'
+            }
+        }
+    } else {
+        echo "Skipping Terraform apply since action is not set to 'apply'"
+    }
+}
