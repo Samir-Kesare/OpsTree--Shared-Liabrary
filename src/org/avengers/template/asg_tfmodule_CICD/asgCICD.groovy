64e1de5b910f27a28c638c61984dc1d5cd526dbb
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-03-20 09:43:19 +0530

    Create asgCICD.groovy

---
+++ src/org/avengers/template/asg_tfmodule_CICD/asgCICD.groovy
@@ -0,0 +1,30 @@
+package org.avengers.template.terraformCI
+
+import org.avengers.common.*
+import org.avengers.terraform_CICD.*
+
+def call(String url, String creds, String branch, String rootPath, String childPath, String reportName1, String reportName2){
+
+    variablization = new action()
+    gitCheckoutPrivate = new GitCheckoutPrivate()
+    initialization = new init()
+    formating = new fmt()
+    validation = new validate()
+    linting = new linting()
+    securitychecks = new checkov()
+    cleanWorkspace = new cleanWorkspace()
+    archiving = new ArchivewithoutClean()
+
+    cleanWorkspace.call()
+    gitCheckoutPrivate.call(url, creds, branch)
+    variablization.call(rootPath, childPath)
+    initialization.call(rootPath, childPath)
+    formating.call(rootPath, childPath)
+    validation.call(rootPath, childPath)
+    securitychecks.call(rootPath, childPath)
+    linting.call(rootPath, childPath)
+    archiving.call(reportName1)
+    archiving.call(reportName2)
+    // securitychecks.call(rootPath, childPath)
+
+}
