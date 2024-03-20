8420421d820e7901424146f13455caf2dadea0fd
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-03-20 14:15:45 +0530

    Update asg_tfmodule_CD.groovy

--- src/org/avengers/asg_tfmodule_CICD/asg_tfmodule_CD.groovy
+++ src/org/avengers/asg_tfmodule_CICD/asg_tfmodule_CD.groovy
@@ -5,7 +5,7 @@ def call(String gitpath, String creds, String tagVersion) {
             script {
                 // Push the tagged version to the remote repository
                 withCredentials([usernamePassword(credentialsId: "${creds}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-                    sh "git push https://username:${PASSWORD}@github.com/${gitpath} v${tagVersion}"
+                    sh "git push https://username:${PASSWORD}@github.com/'${gitpath}' v${tagVersion}"
                 }
             }
         }
