41840abe521e472a13b645581def6e8120eac17f
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-03-20 12:01:45 +0530

    Update asg_tfmodule_CI.groovy

--- src/org/avengers/asg_tfmodule_CICD/asg_tfmodule_CI.groovy
+++ src/org/avengers/asg_tfmodule_CICD/asg_tfmodule_CI.groovy
@@ -21,10 +21,10 @@ def call(String rootPath, String childPath, String tagVersion) {
                 sh "python3 -m pip install checkov"
                 sh 'echo "export PATH=\"`python3 -m site --user-base`/bin:\$PATH\"" >> ~/.bashrc'
                 sh "source ~/.bashrc"
-                sh 'export PATH="$HOME/.local/bin:$PATH"'
+                
                 sh "sudo apt install pipenv -y"
                 sh "pip install checkov"
-                sh "/var/lib/jenkins/.local/bin/checkov -d . -s"
+                
             }
             // Stage to run Checkov for Terraform
             sh "cd ${rootPath}/${childPath} && /var/lib/jenkins/.local/bin/checkov -d . -s --output-file-path . --skip-path ./tflint_report.jsonֿ"
