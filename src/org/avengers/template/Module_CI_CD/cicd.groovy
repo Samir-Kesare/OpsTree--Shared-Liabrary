ef60eaa459763f7c160926f8bd804240d677c157
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-03-20 22:59:59 +0530

    Update cicd.groovy

--- src/org/avengers/template/Module_CI_CD/cicd.groovy
+++ src/org/avengers/template/Module_CI_CD/cicd.groovy
@@ -1,3 +1,21 @@
+// package org.avengers.template.Module_CI_CD
+
+
+// import org.avengers.common.*
+// import org.avengers.Module_CI_CD.*
+
+// def call(String url, String creds, String branch, String rootPath, String childPath, String tagVersion, String gitpath){
+
+    
+//     gitCheckoutPrivate = new GitCheckoutPrivate()
+//     ci = new ci()
+//     cd = new cd()
+    
+//     gitCheckoutPrivate.call(url, creds, branch)
+//     ci.call(rootPath, childPath, tagVersion)
+//     cd.call(gitpath, creds, tagVersion)
+// }
+
 package org.avengers.template.Module_CI_CD
 
 
