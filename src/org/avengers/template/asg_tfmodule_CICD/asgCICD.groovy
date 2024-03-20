634ca01f5c10f279df3cb2fa10d6ff411d7cf7b2
Snatak-SamirKesare <156056570+Snatak-SamirKesare@users.noreply.github.com>
2024-03-20 13:52:29 +0530

    Update asgCICD.groovy

--- src/org/avengers/template/asg_tfmodule_CICD/asgCICD.groovy
+++ src/org/avengers/template/asg_tfmodule_CICD/asgCICD.groovy
@@ -4,13 +4,14 @@ package org.avengers.template.asg_tfmodule_CICD
 import org.avengers.common.*
 import org.avengers.asg_tfmodule_CICD.*
 
-def call(String url, String creds, String branch, String rootPath, String childPath, String tagVersion){
+def call(String url, String creds, String branch, String rootPath, String childPath, String tagVersion, String gitpath){
 
     
     gitCheckoutPrivate = new GitCheckoutPrivate()
     asg_tfmodule_CI = new asg_tfmodule_CI()
-    
+    asg_tfmodule_CD = new asg_tfmodule_CD()
     
     gitCheckoutPrivate.call(url, creds, branch)
     asg_tfmodule_CI.call(rootPath, childPath, tagVersion)
+    asg_tfmodule_CD.call(gitpath, creds, tagVersion)
 }
