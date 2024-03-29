8bbdde3695beb4695f929a1b4388e0f6790976ba
vyadavP7 <156056349+vyadavP7@users.noreply.github.com>
2024-03-29 23:12:25 +0530

    Update terragrunt_stages.groovy

--- src/org/avengers/terragrunt/terragrunt_stages.groovy
+++ src/org/avengers/terragrunt/terragrunt_stages.groovy
@@ -1,1 +1,10 @@
+package org.avengers.java.dast
 
+def call(String zapVersion, String currentWorkspace) {
+stage('Run ZAP Scan') {
+            script {
+                 // Start ZAP and perform the scan
+                  sh "${currentWorkspace}/ZAP_${zapVersion}/zap.sh -cmd -port 8082 -quickurl http://174.129.170.198:8080/swagger-ui/index.html -quickout ${currentWorkspace}/results.html"
+                }
+            }
+        }
