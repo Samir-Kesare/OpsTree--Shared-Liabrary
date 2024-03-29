33f05a904418cb80bc7d458f326715add5c2adac
vyadavP7 <156056349+vyadavP7@users.noreply.github.com>
2024-03-29 16:56:31 +0530

    Update terragrunt.groovy

--- src/org/avengers/template/terragrunt/terragrunt.groovy
+++ src/org/avengers/template/terragrunt/terragrunt.groovy
@@ -1,1 +1,13 @@
 package org.avengers.template.terragrunt
+
+def terragruntInit() {
+    sh 'terragrunt init'
+}
+
+def terragruntPlan() {
+    sh 'terragrunt plan'
+}
+
+def terragruntApply() {
+    sh 'terragrunt apply --auto-approve'
+}
