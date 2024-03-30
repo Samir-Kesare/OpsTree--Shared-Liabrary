a9032015acbbbcb2c7e391a6b7972f71f71a89ef
vyadavP7 <156056349+vyadavP7@users.noreply.github.com>
2024-03-31 01:45:26 +0530

    Update terragrunt.groovy

--- src/org/avengers/template/terragrunt/terragrunt.groovy
+++ src/org/avengers/template/terragrunt/terragrunt.groovy
@@ -3,21 +3,31 @@ package org.avengers.template.terragrunt
 import org.avengers.common.*
 import org.avengers.terragrunt.TerragruntUtils
 
-def call(String url, String creds, String branch, String rootPath, String childPath, String executeDestroy) {
+def call(String url, String creds, String branch, String rootPath, String childPath, String action) {
     def utils = new TerragruntUtils()
     gitCheckoutPrivate = new GitCheckoutPrivate()
-    // approval = new approval()
-    
-    // approval.call
-    gitCheckoutPrivate.call(url, creds, branch)
-    
-    utils.init(rootPath, childPath)
-    utils.plan(rootPath, childPath)
-    utils.apply(rootPath, childPath)
+    def approval = new approval()
+    def applyMsg = 'Do you want to apply the changes ?'
+    def destroyMsg = 'Do you want to destroy the changes ?'
 
+    
+    gitCheckoutPrivate.call(url, creds, branch)
+ 
     if (executeDestroy == 'Yes') {
         utils.destroy(rootPath, childPath)
     } else (executeDestroy == 'No') {
+        error("Invalid action specified: ${action}")
+    }
+
+    if (action == 'apply') {
+        utils.init(rootPath, childPath)
+        utils.plan(rootPath, childPath)
+        approval.call(applyMsg)
+        utils.apply(rootPath, childPath)
+    } else if (action == 'destroy') {
+        approval.call(destroyMsg)
+        utils.destroy(rootPath, childPath)
+    } else {
         error("Invalid action specified: ${action}")
     }
 
