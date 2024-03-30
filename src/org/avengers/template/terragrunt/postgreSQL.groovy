accdd659a3c173ab704991a281e643144f08fe8c
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-03-31 00:05:17 +0530

    Create postgreSQL.groovy

---
+++ src/org/avengers/template/terragrunt/postgreSQL.groovy
@@ -0,0 +1,24 @@
+package org.avengers.template.terragrunt
+
+import org.avengers.common.*
+import org.avengers.terragrunt.TerragruntUtils
+
+def call(String url, String creds, String branch, String rootPath, String childPath, String action) {
+    def utils = new TerragruntUtils()
+    gitCheckoutPrivate = new GitCheckoutPrivate()
+
+    gitCheckoutPrivate.call(url, creds, branch)
+    
+    utils.init(rootPath, childPath)
+    utils.plan(rootPath, childPath)
+
+
+    if (action == 'Apply') {
+        utils.apply(rootPath, childPath)
+    } else if (action == 'Destroy') {
+        utils.destroy(rootPath, childPath)
+    } else {
+        error("Invalid action specified: ${action}")
+    }
+
+}
