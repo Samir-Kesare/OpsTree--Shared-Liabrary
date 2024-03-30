3b478190485abd365f846f70274470702ee76810
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-03-31 00:45:37 +0530

    Update approval.groovy

--- src/org/avengers/common/approval.groovy
+++ src/org/avengers/common/approval.groovy
@@ -1,7 +1,7 @@
 package org.avengers.common
 
-def call(String msg){
-  stage('Approval') {
-        input message: '${msg}', ok: 'Yes'
-    } 
+def call(String msg) {
+    stage('Approval') {
+        input message: "${msg}", ok: 'Yes'
+    }
 }
