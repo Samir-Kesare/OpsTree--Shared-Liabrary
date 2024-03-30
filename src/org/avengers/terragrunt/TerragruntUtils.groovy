086a385c5399d03cfa2c6b4dfae17cf4794f6ae5
vyadavP7 <156056349+vyadavP7@users.noreply.github.com>
2024-03-30 22:08:20 +0530

    Update TerragruntUtils.groovy

--- src/org/avengers/terragrunt/TerragruntUtils.groovy
+++ src/org/avengers/terragrunt/TerragruntUtils.groovy
@@ -1,21 +1,19 @@
 package org.avengers.terragrunt
 
-class TerragruntUtils {
-    def init(String rootPath, String childPath) {
-        stage('Terragrunt Init') {
-            sh "cd ${rootPath}/${childPath} && terragrunt init"
-        }
+def init(String rootPath, String childPath) {
+    stage('Terragrunt Init') {
+        sh "cd ${rootPath}/${childPath} && terragrunt init"
     }
+}
 
-    def plan(String rootPath, String childPath) {
-        stage('Terragrunt Plan') {
-            sh "cd ${rootPath}/${childPath} && terragrunt plan"
-        }
+def plan(String rootPath, String childPath) {
+    stage('Terragrunt Plan') {
+        sh "cd ${rootPath}/${childPath} && terragrunt plan"
     }
+}
 
-    def apply(String rootPath, String childPath) {
-        stage('Terragrunt Apply') {
-            sh "cd ${rootPath}/${childPath} && terragrunt apply"
-        }
+def apply(String rootPath, String childPath) {
+    stage('Terragrunt Apply') {
+        sh "cd ${rootPath}/${childPath} && terragrunt apply"
     }
 }
