df0b792a1ad95d433b6d7a5dd5ccf7510aa6ec51
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-03-31 00:32:53 +0530

    Update postgreSQL.groovy

--- src/org/avengers/template/terragrunt/postgreSQL.groovy
+++ src/org/avengers/template/terragrunt/postgreSQL.groovy
@@ -7,17 +7,17 @@ def call(String url, String creds, String branch, String rootPath, String childP
     def utils = new TerragruntUtils()
     def gitCheckoutPrivate = new GitCheckoutPrivate()
     def approval = new approval()
-    
+    def applyMsg = 'Do you want to apply the changes ?'
+    def destroyMsg = 'Do you want to destroy the changes ?'
     gitCheckoutPrivate.call(url, creds, branch)
     
-
-
     if (action == 'Apply') {
         utils.init(rootPath, childPath)
         utils.plan(rootPath, childPath)
-        approval.call()        
+        approval.call(applyMsg)        
         utils.apply(rootPath, childPath)
     } else if (action == 'Destroy') {
+        approval.call(destroyMsg)
         utils.destroy(rootPath, childPath)
     } else {
         error("Invalid action specified: ${action}")
