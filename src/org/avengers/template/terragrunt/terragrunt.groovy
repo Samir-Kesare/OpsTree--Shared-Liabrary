20ff8aee51f0448932a3a8f5f35c7717bb97c999
vyadavP7 <156056349+vyadavP7@users.noreply.github.com>
2024-03-31 00:02:30 +0530

    Update terragrunt.groovy

--- src/org/avengers/template/terragrunt/terragrunt.groovy
+++ src/org/avengers/template/terragrunt/terragrunt.groovy
@@ -3,7 +3,7 @@ package org.avengers.template.terragrunt
 import org.avengers.common.*
 import org.avengers.terragrunt.TerragruntUtils
 
-def call(String url, String creds, String branch, String rootPath, String childPath) {
+def call(String url, String creds, String branch, String rootPath, String childPath, String action) {
     def utils = new TerragruntUtils()
     gitCheckoutPrivate = new GitCheckoutPrivate()
 
