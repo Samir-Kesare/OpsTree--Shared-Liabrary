c22fc6e1241ffd17462aa97a83fd8d357f63b2a0
vyadavP7 <156056349+vyadavP7@users.noreply.github.com>
2024-03-30 23:53:45 +0530

    Update terragrunt.groovy

--- src/org/avengers/template/terragrunt/terragrunt.groovy
+++ src/org/avengers/template/terragrunt/terragrunt.groovy
@@ -11,7 +11,18 @@ def call(String url, String creds, String branch, String rootPath, String childP
     
     utils.init(rootPath, childPath)
     utils.plan(rootPath, childPath)
-    utils.apply(rootPath, childPath)
-    utils.destroy(rootPath, childPath)
+
+
+    // Pipeline approval
+    input(message: 'Proceed with applying or destroying infrastructure?', parameters: [choice(name: 'action', choices: ['Apply', 'Destroy'], description: 'Select action')])
+
+    // Conditional block based on user's choice
+    if (params.action == 'Apply') {
+        utils.apply(rootPath, childPath)
+    } else if (params.action == 'Destroy') {
+        utils.destroy(rootPath, childPath)
+    } else {
+        error('Invalid action selected.')
+    }
 
 }
