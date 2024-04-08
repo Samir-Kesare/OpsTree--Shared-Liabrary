2dac2f119e60d646e01d90652151c9d842281fc8
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-04-08 15:43:16 +0530

    Update ansibleRoleCD.groovy

--- src/org/avengers/template/ansibleRole_CD/ansibleRoleCD.groovy
+++ src/org/avengers/template/ansibleRole_CD/ansibleRoleCD.groovy
@@ -3,7 +3,9 @@ package org.avengers.template.ansibleRole_CD
 import org.avengers.common.*
 import org.avengers.ansibleRole_CD.*
 
-def call(String url, String creds, String branch, String gitleaksVersion, String tagVersion, String path, String gitpath){
+// def call(String url, String creds, String branch, String gitleaksVersion, String tagVersion, String path, String gitpath){
+def call(String url, String creds, String branch, String tagVersion, String path, String gitpath){
+ 
   gitCheckoutPrivate = new GitCheckoutPrivate()
   credentialscan = new Credentialscan()
   ansiblelint = new Ansiblelint()
@@ -14,7 +16,7 @@ def call(String url, String creds, String branch, String gitleaksVersion, String
 
   gitCheckoutPrivate.call(url, creds, branch)
   ansiblelint.call(path)
-  credentialscan.call(gitleaksVersion)
+  credentialscan.call()
   syntaxcheck.call(path)
   dryRun.call(path)
   gitTag.call(tagVersion)
