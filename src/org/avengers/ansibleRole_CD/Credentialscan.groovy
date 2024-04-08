2e2f7ea7aa86ee584dd781012ec7c3258e19ec49
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-04-08 15:42:40 +0530

    Update Credentialscan.groovy

--- src/org/avengers/ansibleRole_CD/Credentialscan.groovy
+++ src/org/avengers/ansibleRole_CD/Credentialscan.groovy
@@ -1,10 +1,11 @@
 package org.avengers.ansibleRole_CD
-def call(String gitleaksVersion) {
+// def call(String gitleaksVersion) {
+def call() {
   stage('Credential Scanning') {
             script {
-                sh "wget https://github.com/gitleaks/gitleaks/releases/download/v${gitleaksVersion}/gitleaks_${gitleaksVersion}_linux_x64.tar.gz"
+                // sh "wget https://github.com/gitleaks/gitleaks/releases/download/v${gitleaksVersion}/gitleaks_${gitleaksVersion}_linux_x64.tar.gz"
                 // Extract Gitleaks
-                sh "tar xvzf gitleaks_${gitleaksVersion}_linux_x64.tar.gz"
+                // sh "tar xvzf gitleaks_${gitleaksVersion}_linux_x64.tar.gz"
                 sh "./gitleaks detect -r credScanReport.json"
             }
         }
