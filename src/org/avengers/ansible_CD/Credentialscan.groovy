371948e0494ecd7e6ea5676ec920ec05c71265dd
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-04-09 11:13:30 +0530

    Update Credentialscan.groovy

--- src/org/avengers/ansible_CD/Credentialscan.groovy
+++ src/org/avengers/ansible_CD/Credentialscan.groovy
@@ -1,11 +1,27 @@
 package org.avengers.ansible_CD
-def call(String gitleaksVersion) {
+// def call(String gitleaksVersion) {
+//   stage('Credential Scanning') {
+//             script {
+//               try {
+//                 sh "wget https://github.com/gitleaks/gitleaks/releases/download/v${gitleaksVersion}/gitleaks_${gitleaksVersion}_linux_x64.tar.gz"
+//                 // Extract Gitleaks
+//                 sh "tar xvzf gitleaks_${gitleaksVersion}_linux_x64.tar.gz"
+//                 sh "./gitleaks detect -r credScanReport.json"
+//               }
+//               catch (Exception e)  {
+//               sh ' echo " Please check the cred scan report " '
+//               }
+              
+//             }
+//         }
+// }
+def call() {
   stage('Credential Scanning') {
             script {
               try {
-                sh "wget https://github.com/gitleaks/gitleaks/releases/download/v${gitleaksVersion}/gitleaks_${gitleaksVersion}_linux_x64.tar.gz"
+                // sh "wget https://github.com/gitleaks/gitleaks/releases/download/v${gitleaksVersion}/gitleaks_${gitleaksVersion}_linux_x64.tar.gz"
                 // Extract Gitleaks
-                sh "tar xvzf gitleaks_${gitleaksVersion}_linux_x64.tar.gz"
+                // sh "tar xvzf gitleaks_${gitleaksVersion}_linux_x64.tar.gz"
                 sh "./gitleaks detect -r credScanReport.json"
               }
               catch (Exception e)  {
