a71cbeeef280538c0f698947e2a620359cc01439
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-04-23 01:50:01 +0530

    Update dockerCI.groovy

--- src/org/avengers/docker/CI/dockerCI.groovy
+++ src/org/avengers/docker/CI/dockerCI.groovy
@@ -15,7 +15,7 @@ def createImage(String imageName, String dockerfilePath) {
 def trivyCheck(String imageName) {
     stage('trivy Scan'){
     sh """
-       trivy image -q ${imageName}:latest
+       sudo trivy image -q ${imageName}:latest
     """    
   }
 }
