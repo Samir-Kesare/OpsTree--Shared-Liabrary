c26542d93ba0d5336e7de485cd1467b96d5f472a
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-04-22 21:31:28 +0530

    Update terragruntUtils.groovy

--- src/org/avengers/blue_green/terragruntUtils.groovy
+++ src/org/avengers/blue_green/terragruntUtils.groovy
@@ -8,7 +8,8 @@ def init(String rootPath, String childPath) {
 
 def plan(String rootPath, String childPath, String extraVars) {
     stage('Terragrunt Plan') {
-        sh "cd ${rootPath}/${childPath} && terragrunt plan  --terragrunt-disable-command-validation  ${extraVars}"
+        sh 'cd ${rootPath}/${childPath} && terragrunt plan -var="traffic_distribution=blue"'
+
     }
 }
 
