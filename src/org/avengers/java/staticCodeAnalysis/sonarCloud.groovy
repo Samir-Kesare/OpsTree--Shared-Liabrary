39277256d7719fd6a21590b7a2895a730eb6ad09
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-04-22 18:49:09 +0530

    Update sonarCloud.groovy

--- src/org/avengers/java/staticCodeAnalysis/sonarCloud.groovy
+++ src/org/avengers/java/staticCodeAnalysis/sonarCloud.groovy
@@ -4,7 +4,8 @@ def call(String organization, String projectKey) {
   
   stage('Static Code Analysis'){
         withSonarQubeEnv(installationName: 'sq1') { 
-          sh './mvnw org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.0.2155:sonar -Dsonar.organization=${organization} -Dsonar.projectKey=${projectKey}'
+        //   sh './mvnw org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.0.2155:sonar -Dsonar.organization=${organization} -Dsonar.projectKey=${projectKey}'
+          sh "./mvnw org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.0.2155:sonar -Dsonar.organization=${organization} -Dsonar.projectKey=${projectKey}"
         }    
   }
 }
