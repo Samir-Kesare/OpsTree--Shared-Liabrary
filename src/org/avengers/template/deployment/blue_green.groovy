f7e8738a1466f7392be02122af82d8eb173c4745
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-04-22 11:48:01 +0530

    Create blue_green.groovy

---
+++ src/org/avengers/template/deployment/blue_green.groovy
@@ -0,0 +1,28 @@
+package org.avengers.template.deployment
+
+import org.avengers.common.*
+import org.avengers.blue_green.terragruntUtils.groovy
+
+def call(String url, String creds, String branch, String rootPath, String childPath, String action) {
+    def utils = new TerragruntUtils()
+    gitCheckoutPrivate = new GitCheckoutPrivate()
+    def approval = new approval()
+    def applyMsg = 'Do you want to apply the changes ?'
+    def destroyMsg = 'Do you want to destroy the changes ?'
+
+    
+    gitCheckoutPrivate.call(url, creds, branch)
+
+    if (action == 'apply') {
+        utils.init(rootPath, childPath)
+        utils.plan(rootPath, childPath)
+        approval.call(applyMsg)
+        utils.apply(rootPath, childPath)
+    } else if (action == 'destroy') {
+        approval.call(destroyMsg)
+        utils.destroy(rootPath, childPath)
+    } else {
+        error("Invalid action specified: ${action}")
+    }
+
+}
