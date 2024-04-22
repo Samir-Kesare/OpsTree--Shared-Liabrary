e8530efa1b2e1dba6099d001524c078db75e6df2
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-04-23 02:34:09 +0530

    updated  dockerCI

--- src/org/avengers/docker/CI/dockerCI.groovy
+++ src/org/avengers/docker/CI/dockerCI.groovy
@@ -16,7 +16,7 @@ def createImage(String imageName, String dockerfilePath) {
 def trivyCheck() {
     stage('trivy Scan'){
     sh """
-      sudo trivy image -q --severity HIGH,CRITICAL --exit-code 1 --format template --template '{{- \$critical := 0 }}{{- \$high := 0 }}{{- range . }}{{- range .Vulnerabilities }}{{- if  eq .Severity "CRITICAL" }}{{- \$critical = add \$critical 1 }}{{- end }}{{- if  eq .Severity "HIGH" }}{{- \$high = add \$high 1 }}{{- end }}{{- end }}{{- end }}Critical: {{ \$critical }}, High: {{ \$high }}' salary-image:latest -o trivy-results.json
+      sudo trivy image -q --severity HIGH,CRITICAL --exit-code 1 --format template --template '{{- \$critical := 0 }}{{- \$high := 0 }}{{- range . }}{{- range .Vulnerabilities }}{{- if  eq .Severity "CRITICAL" }}{{- \$critical = add \$critical 1 }}{{- end }}{{- if  eq .Severity "HIGH" }}{{- \$high = add \$high 1 }}{{- end }}{{- end }}{{- end }}Critical: {{ \$critical }}, High: {{ \$high }}' -o trivy-results.json salary-image:latest 
       
       awk 'BEGIN { FS="[:,]"; OFS="," }
       {
