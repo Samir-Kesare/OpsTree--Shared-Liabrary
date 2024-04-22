4e8dddabb641b449771626c57e40fc306966b3ea
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-04-22 14:39:06 +0530

    Update terragruntUtils.groovy

--- src/org/avengers/blue_green/terragruntUtils.groovy
+++ src/org/avengers/blue_green/terragruntUtils.groovy
@@ -23,3 +23,18 @@ def destroy(String rootPath, String childPath) {
         sh "cd ${rootPath}/${childPath} && terragrunt destroy -auto-approve"
     }
 }
+
+def healthCheck(String rootPath, String childPath) {
+        stage('TG Health Check'){
+                    def green_tg_arn = sh(
+                        returnStdout: true,
+                        script: 'cd ${rootPath}/${childPath} && terragrunt output final_target_group_arn').trim()
+                    env.GREEN_TG_ARN = green_tg_arn
+                    }
+        stage('Print DNS Name'){
+                    sh """
+                        echo ${env.GREEN_TG_ARN} 
+                        aws elbv2 describe-target-health --target-group-arn ${env.GREEN_TG_ARN} | grep State
+                    """
+        }
+}
