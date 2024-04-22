1a782ce4fecb5e358afc37b04f5d7c364a22cd06
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-04-23 02:41:57 +0530

    abc

--- src/org/avengers/docker/CI/dockerCI.groovy
+++ src/org/avengers/docker/CI/dockerCI.groovy
@@ -18,20 +18,17 @@ def trivyCheck() {
     sh """
       sudo trivy image -q --severity HIGH,CRITICAL --exit-code 1 --format template --template '{{- $$critical := 0 }}{{- $$high := 0 }}{{- range . }}{{- range .Vulnerabilities }}{{- if  eq .Severity "CRITICAL" }}{{- $$critical = add $$critical 1 }}{{- end }}{{- if  eq .Severity "HIGH" }}{{- $$high = add $$high 1 }}{{- end }}{{- end }}{{- end }}Critical: {{ $$critical }}, High: {{ $$high }}' -o trivy-results.json salary-image:latest 
     """
-    
-    sh"""  
-      awk 'BEGIN { FS="[:,]"; OFS="," }
-      {
-            for (i = 1; i <= NF; i += 2) {
-                gsub(/ /, "", $$i); # Remove spaces from keys
-                header = (header ? header OFS : "") $$i;
-                value = (value ? value OFS : "") $$i+1;
-            }
-            print header > "mi.csv";
-            print value >> "mi.csv";
-      }' trivy-results.json
-
-      cat mi.csv
-    """    
+    sh '''
+    awk 'BEGIN { FS="[:,]"; OFS="," }
+    {
+        for (i = 1; i <= NF; i += 2) {
+            gsub(/ /, "", $i); # Remove spaces from keys
+            header = (header ? header OFS : "") $i;
+            value = (value ? value OFS : "") $(i+1);
+        }
+        print header > "mi.csv";
+        print value >> "mi.csv";
+    }' trivy-results.json
+    '''   
   }
 }
