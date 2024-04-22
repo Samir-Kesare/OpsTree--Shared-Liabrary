804346c9f0271cc438766ab66e0e9a8d676be970
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-04-22 21:43:02 +0530

    Update terragruntUtils.groovy

--- src/org/avengers/blue_green/terragruntUtils.groovy
+++ src/org/avengers/blue_green/terragruntUtils.groovy
@@ -1,42 +1,47 @@
 package org.avengers.blue_green
 
-def init(String rootPath, String childPath) {
+def init(String rootPath, String greenPath) {
     stage('Terragrunt Init') {
-        sh "cd ${rootPath}/${childPath} && terragrunt run-all --terragrunt-non-interactive  init"
+        sh "cd ${rootPath}/${greenPath} && terragrunt run-all --terragrunt-non-interactive  init"
     }
 }
 
-def plan(String rootPath, String childPath) {
+def plan(String rootPath, String greenPath) {
     stage('Terragrunt Plan') {
-        sh "cd ${rootPath}/${childPath} && terragrunt plan -var='traffic_distribution=blue'"
+        sh "cd ${rootPath}/${greenPath} && terragrunt plan -var='traffic_distribution=blue'"
 
     }
 }
 
-def createGreenTG(String rootPath, String childPath) {
+def createGreenTG(String rootPath, String greenPath) {
     stage('Create Green TG') {
-       sh "cd ${rootPath}/${childPath} && terragrunt apply -auto-approve -var='traffic_distribution=blue'"
+       sh "cd ${rootPath}/${greenPath} && terragrunt apply -auto-approve -var='traffic_distribution=blue'"
     }
 }
 
-def destroy(String rootPath, String childPath) {
+def destroy(String rootPath, String greenPath) {
     stage('Terragrunt Destroy') {
-        sh "cd ${rootPath}/${childPath} && terragrunt destroy -auto-approve"
+        sh "cd ${rootPath}/${greenPath} && terragrunt destroy -auto-approve"
     }
 }
 
-def healthCheck(String rootPath, String childPath) {
+def healthCheck(String rootPath, String greenPath) {
         stage('TG Health Check'){
                     def green_tg_arn = sh(
                         returnStdout: true,
-                        script: "cd ${rootPath}/${childPath} && terragrunt output final_target_group_arn").trim()
+                        script: "cd ${rootPath}/${greenPath} && terragrunt output final_target_group_arn").trim()
                     env.GREEN_TG_ARN = green_tg_arn
                     }
     
 }
-def deployGreen(String rootPath, String childPath) {
+def deployGreen(String rootPath, String greenPath) {
     stage('Deploy Green') {
-        sh "cd ${rootPath}/${childPath} && terragrunt apply -auto-approve -var='traffic_distribution=green'"
+        sh "cd ${rootPath}/${greenPath} && terragrunt apply -auto-approve -var='traffic_distribution=green'"
     }
 }
-
+def destroyBlue(String rootPath, String bluePath) {
+    stage('Destroy Blue') {
+        // sh "cd ${rootPath}/${bluePath} && terragrunt init -auto-approve -var='traffic_distribution=green'"
+        sh "cd ${rootPath}/${bluePath} && terragrunt apply -auto-approve -var='min_size=0' -var='max_size=0' -var='desired_capacity=0'"
+    }
+}
