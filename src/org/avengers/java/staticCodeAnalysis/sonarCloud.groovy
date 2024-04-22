66724850642e4cdb512ebd68282f0f5b3e446475
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-04-22 18:16:03 +0530

    Update sonarCloud.groovy

--- src/org/avengers/java/staticCodeAnalysis/sonarCloud.groovy
+++ src/org/avengers/java/staticCodeAnalysis/sonarCloud.groovy
@@ -1,1 +1,9 @@
+package org.avengers.java.staticCodeAnalysis
 
+def call(String organization, String projectKey) {
+  stage('Static Code Analysis'){
+        withSonarQubeEnv(installationName: 'sq1') { 
+          sh 'mvn clean verify sonar:sonar -Dsonar.organization=${organization} -Dsonar.projectKey=${projectKey}'
+        }    
+  }
+}
