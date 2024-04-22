4df740fc229c606f1948273690b8e19d914a4b16
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-04-23 02:31:42 +0530

    updated dockerCI

--- src/org/avengers/docker/CI/dockerCI.groovy
+++ src/org/avengers/docker/CI/dockerCI.groovy
@@ -21,9 +21,9 @@ def trivyCheck() {
       awk 'BEGIN { FS="[:,]"; OFS="," }
       {
             for (i = 1; i <= NF; i += 2) {
-                gsub(/ /, "", $i); # Remove spaces from keys
-                header = (header ? header OFS : "") $i;
-                value = (value ? value OFS : "") $i+1;
+                gsub(/ /, "", \$i); # Remove spaces from keys
+                header = (header ? header OFS : "") \$i;
+                value = (value ? value OFS : "") \$i+1;
             }
             print header > "mi.csv";
             print value >> "mi.csv";
