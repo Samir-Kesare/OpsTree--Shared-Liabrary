1ef380c4cf0f5666fc1dfa9f1ca5ef2d2e084d10
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-04-23 01:04:01 +0530

    Update dockerCI.groovy

--- src/org/avengers/docker/CI/dockerCI.groovy
+++ src/org/avengers/docker/CI/dockerCI.groovy
@@ -1,13 +1,12 @@
 package org.avengers.docker.CI
 
 def createImage(String imageName, String dockerfilePath) {
-   // sudo docker build -t ${imageName} ${dockerfilePath}
-    
+    // sudo docker build -t ${imageName} ${dockerfilePath}  
+   // sudo docker rmi -f df53b9875ae3
+  // sudo docker images
   stage('Create Docker Image'){
     sh """
-      sudo docker images
-      sudo docker rmi -f df53b9875ae3
-      sudo docker images
+      docker images
     """    
   }
 }
