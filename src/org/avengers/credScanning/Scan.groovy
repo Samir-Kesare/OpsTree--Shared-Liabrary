8037ec8c5c32f2d7ac186f62c4c3dd92c2869408
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-04-24 01:03:47 +0530

    updated scan.groovy

--- src/org/avengers/credScanning/Scan.groovy
+++ src/org/avengers/credScanning/Scan.groovy
@@ -2,9 +2,32 @@ package org.avengers.credScanning
 
 def call(String reportName) {
   stage('Gitleaks Scan') {
-        script {
-           sh "./gitleaks detect -r ${reportName}"
-        }
+        // script {
+        //    sh "./gitleaks detect -r ${reportName}"
+        // }
+        sh'''
+          ./gitleaks detect --exit-code 1 --report-format json --report-path gitleaks.json -v
+        '''
+        sh'''
+          jq -r 'group_by(.RuleID) | map({RuleID: .[0].RuleID, Count: length}) | (map(.RuleID) | @csv), (map(.Count) | @csv)' gitleaks.json | sed 's/"//g' > cred_scanner.csv
+        '''
+        sh'''
+          cat cred_scanner.csv
+        '''
+        sh'''
+          export application=ot-demo-ms
+          export environment=dev
+          export service=salary-api
+          export organization=bp
+          export source_key=gitleak 
+          export report_file_path=null
+          export base64EncodedResponse=`base64 cred_scanner.csv`
+          envsubst < mi.template > cred_scanner.mi
+          
+          cat cred_scanner.mi
+           
+          curl -d "@cred_scanner.mi" -X POST  -H "Content-Type: application/json" http://34.131.67.101:9001/api/v1/maturity_dashboard/maturity_metrices/
+        '''
     }
-  
+
 }
